b84d8b7f33f9788f942e6b6fd93dc0b4257d8372
vicfriedman <victoria.m.friedman@gmail.com>
2015-12-21 12:09:42 -0500

    lab

---
+++ js/solution.js
@@ -0,0 +1,11 @@
+function happyHolidays(){
+  return "Happy Holidays!!";
+}
+
+function happyHolidaysWithName(name){
+  return "Happy Holidays, " + name;
+}
+
+function happyAnyHoliday(holiday, name){
+  return "Happy " + holiday" + ", " " + name;
+}
