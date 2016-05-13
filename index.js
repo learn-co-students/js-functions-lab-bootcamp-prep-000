1be1e706133175c8a045eedea3273152146fc38b
Charles Pletcher <cpletcher2@gmail.com>
2016-05-13 16:39:13 -0400

    Fix tests, add solution

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ function happyHolidaysTo(name) {
   return `Happy holidays, ${name}!`
 }
 
-function happyCustomHolidayTo(holiday, name) {
+function happyHolidayTo(holiday, name) {
   return `Happy ${holiday}, ${name}!`
 }
 
