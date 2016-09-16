2b1cd9d49df85dc7c1e9fa4aaea4c03a9de5a6c3
Alexandre Kim <alexandrekim93@gmail.com>
2016-09-16 04:47:02 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // write your code below!
+function happyHolidays(){
+  return "Happy holidays!"
+}
+
+happyHolidays(); // 'Happy holidays!'
+
+function happyHolidaysTo(name) {
+  return "Happy holidays, " + `${name}` + "!";
+}
+
+happyHolidaysTo("Alex"); // 'Happy holidays, Alex!'
+
+function happyHolidayTo(holiday, name) {
+  return "Happy " + `${holiday}` + ", " + `${name}` + "!";
+}
+
+happyHolidayTo("Thanksgiving", "Alex");
+
+function holidayCountdown(holiday, days) {
+  return "It's " + `${days}` + " days until " + `${holiday}` + "!";
+}
+
+holidayCountdown("Chirstmas", 25);
