be5d6c852c3437518d1c99fe5951b68f2090b0ed
Kyung Song <kyunghan.song@gmail.com>
2016-09-24 20:05:14 +0000

    Done.

--- index.js
+++ index.js
@@ -3,16 +3,15 @@ function happyHolidays(){
 
     return "Happy holidays!";
 }
-/*
+
 function happyHolidaysTo(name){
-    return "Happy holidays, " +${name} +"!";
+    return `Happy holidays, ${name}!`;
 }
 
 function happyHolidayTo(holiday, name){
-    return "Happy "+${holiday}+","+${name}+"!";
+    return `Happy ${holiday}, ${name}!`;
 }
 
 function holidayCountdown(holiday,days){
-    return "It's "+${days}+" days until "+${holiday}+"!";
+    return `It's ${days} days until ${holiday}!`;
 }
-*/
