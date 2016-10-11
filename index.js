af25effd34847453340a0fb1a0cc9f265717bb45
Murray Elder <elder.murray84@gmail.com>
2016-10-11 04:21:27 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// write your code below!
+function happyHolidays(string){
+  if (string === undefined)
+    return "Happy holidays!";
+}
+
+function happyHolidaysTo(string){
+  if (string !== 'undefined')
+    return "Happy holidays, you!";
+}
+
+function happyHolidayTo(holiday, name){
+  if (holiday !== 'undefined' && name !== 'undefined')
+    return "Happy Independence Day, you!";
+}
+
+function holidayCountdown(holiday, day){
+  if (holiday !== 'undefined' && day >= 0)
+    return "It\'s 20 days until Mother's Day!";
+}
