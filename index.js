2caaebca553a6caa0cacbf19a04133954726383c
jason brown <j.grant.brown@gmail.com>
2016-10-17 18:10:13 +0000

    Done.

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 // write your code below!
-function happyHolidays(){
+/*function happyHolidays(){
   return "Happy holidays!"
 }
-/*function happyHolidaysTo(name){
+*/
+function happyHolidaysTo(name){
   return `Happy holidays, ${name}!`
 }
 function happyHolidayTo(holiday,name){
