04da12800145c503413bd17c36815f5fb7b3489b
Elizabeth Hall <elizabethhall205@gmail.com>
2016-10-22 14:47:50 +0000

    Done.

--- index.js
+++ index.js
@@ -6,3 +6,11 @@ function happyHolidays() {
 function happyHolidaysTo(name) {
 		return `Happy holidays, ${name}!`
 }
+
+function happyHolidayTo(holiday, name) {
+	return `Happy ${holiday}, ${name}!`
+}
+
+function holidayCountdown(holiday, days) {
+	return `It's ${days} days until ${holiday}!`
+}
