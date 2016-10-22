3cd41e6253e7be887b054b0962a8e8f1afc37abe
Elizabeth Hall <elizabethhall205@gmail.com>
2016-10-22 14:38:14 +0000

    Done.

--- index.js
+++ index.js
@@ -3,6 +3,6 @@ function happyHolidays() {
 	return "Happy holidays!"
 }
 
-function happyHolidaysTo(you) {
-	return `Happy holidays, ${name}!`
+function happyHolidaysTo(name) {
+		return `Happy holidays, ${name}!`
 }
