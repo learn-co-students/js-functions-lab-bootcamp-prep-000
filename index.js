d3600ae0c0202759b09563a8f6d6ef9a4af9f187
Matthew White <matthew.white859@gmail.com>
2016-10-28 19:52:17 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// write your code below!
+"use strict";
+function happyHolidays() {
+    return "Happy holidays!";
+}
+function happyHolidaysWithName(name) {
+    return "Happy Holidays, " + name;
+}
+function happyAnyHoliday(holiday, name) {
+    return "Happy " + holiday + " " + name;
+}
+function holidayCountDown(holiday, days) {
+    return "It's " + days + " days till " + holiday;
+}
