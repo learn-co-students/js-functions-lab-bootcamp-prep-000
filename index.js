fefff729f368be4b645e5562a71d29b5f3da664b
Finley Bomer-Lawson <finley@flatironschool.com>
2016-11-11 22:42:50 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // write your code below!
+function happyHolidays()  {
+  return "Happy holidays!"
+}
+
+function happyHolidaysTo() {
+  console.log('you')
+  return "Happy holidays, you!"
+}
+
+function happyHolidayTo()  {
+  console.log('you')
+  return "Happy Independence Day, you!"
+}
+
+function holidayCountdown()  {
+  console.log('you')
+  return "It's 20 days until Mother's Day!"
+}
