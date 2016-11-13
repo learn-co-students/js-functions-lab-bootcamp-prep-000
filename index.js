ffae5e389b76fa449995b39fcb4b1aa83593d73a
Justin Zuchowski <justin.zuchowski@gmail.com>
2016-11-13 16:34:16 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // write your code below!
+
+function happyHolidays() {
+  return "Happy holidays!";
+}
+/*function happyHolidaysTo(name) {
+  var name = "you";
+  return `Happy holidays, ${name}!`;
+  //When using  the `${variable}` you need to put the ticks around the whole return.
+}
+*/
+function happyHolidaysTo(name) {
+  return `Happy holidays, ${name}!`;
+ };
+
+ function happyHolidayTo(holiday, name) {
+   return `Happy ${holiday}, ${name}!`;
+ }
+
+function holidayCountdown(holiday, days) {
+  return `It\'s ${days} days until ${holiday}!`
+}
