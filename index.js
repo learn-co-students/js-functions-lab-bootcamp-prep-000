c6cff088dad732234864b8a4ba346cadcfb2f536
Kyle Hughes <blinded93@gmail.com>
2016-11-21 20:23:21 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // write your code below!
+function happyHolidays() {
+  return "Happy holidays!"; // general holidays greeting
+}
+
+function happyHolidaysTo(name) {
+  return `Happy holidays, ${name}!`; // targeted holiday greeting
+}
+
+function happyHolidayTo(holiday, name) {
+  return `Happy ${holiday}, ${name}!`; // targeted greeting for a specific holiday
+}
+
+function holidayCountdown(holiday, days) {
+  return `It'\s ${days} days until ${holiday}!`; // countdown until a specific holiday
+}
