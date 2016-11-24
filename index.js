d9b6d3220a6fb06cc6b8d1fddf7f1680595434e8
Pak Belalang <opblang@yandex.com>
2016-11-24 09:48:55 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // write your code below!
+var happyHolidays = () => 'Happy holidays!'
+var happyHolidayTo = (x, y) => `Happy ${x}, ${y}!`
+var happyHolidaysTo = (x) => `Happy holidays, ${x}!`
+var holidayCountdown = (x, y) => `It's ${y} days until ${x}!`
