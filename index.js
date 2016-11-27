b06fe2a92c1e44318e0bc1cccfd3e27ef45d1ede
Rudy Zapata <rstar026@yahoo.com>
2016-11-27 22:38:08 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // write your code below!
+function happyHolidays(string) {
+  return "Happy holidays!"
+}
+
+function happyHolidaysTo(name){
+if (name === undefined){
+    name = you} {
+     return "Happy holidays, ${name}!"
+   }
+}
