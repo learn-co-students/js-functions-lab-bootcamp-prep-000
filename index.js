60bb9679db8c5cdcaa04f600249f7dbb893674d8
Josh Cone <joshcone@googlemail.com>
2016-11-30 19:42:00 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // write your code below!
+function happyHolidays() {
+  return "Happy holidays!"
+}
+function happyHolidaysTo(Josh) {
+  return "Happy holidays, " + Josh + "!"
+}
+function happyHolidayTo(xMas, Josh) {
+  return "Happy " + xMas + ", " + Josh + "!"
+}
+function holidayCountdown(xMas, days) {
+  return `It\'s ` + days +  ` days until ` + xMas + `!`
+}
