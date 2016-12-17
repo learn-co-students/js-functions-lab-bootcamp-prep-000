3e8fa9dfe467d5a63fcd204f1fd405ab249f1e91
Julio Guzman <julio_guzman@ymail.com>
2016-12-17 18:48:29 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // write your code below!
+
+// Number 1.) A function with no para. and returns happy holidays.
+function happyHolidays() {
+  return "Happy holidays!"
+}
+
+// Number 2.) Function with parameter of (name) and return "Happpy Holidays (name)".
+function happyHolidaysTo(name) {
+  return `Happy holidays, ${name}!`
+}
+
+// Number 3.) Function with 2 parameters (holiday) and (name). Return "Happy (holiday), (name)".
+function happyHolidayTo(holiday, name) {
+  return `Happy ${holiday}, ${name}!`
+}
+
+// Number 4.) FUnction holidayCountdown with two parameters (days) and (holiday). Returns "Its (days) days until (holiday)".
+function holidayCountdown(holiday, days) {
+  return `It\'s ${days} days until ${holiday}!`
+}
