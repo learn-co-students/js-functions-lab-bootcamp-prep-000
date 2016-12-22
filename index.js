f94eaebbbcd6693d14123d7350d8232a42905e31
IKSHVAKU SINGAPURAM <ixs21870@ucmo.edu>
2016-12-22 05:52:03 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// write your code below!
+function happyHolidays(){
+
+  return "Happy holidays!";
+}// write your code below!
+
+function happyHolidaysTo(name){
+
+  return `Happy holidays, ${name}!`;
+}// write your code below!
+
+
+function happyHolidayTo(holiday,name){
+
+  return `Happy ${holiday}, ${name}!`;
+}// write your code below!
+
+
+function holidayCountdown(days,holiday){
+
+  return `It's ${holiday} days until ${days}!`;
+}// write your code below!
