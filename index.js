fae2e9e77e1b175619d24aa9bd6304ecd8fe586a
Penn Wu <phw.dental@gmail.com>
2017-02-07 11:52:56 +0000

    Done.

--- index.js
+++ index.js
@@ -5,9 +5,9 @@ function happyHolidays() {
 function happyHolidaysTo(name) {
   return `Happy holidays, ${name}!`
 }
-function happyHolidaysTo(holiday, name) {
+function happyHolidayTo(holiday, name) {
   return `Happy ${holiday}, ${name}!`
 }
-function holidayCountdown(days, holiday) {
+function holidayCountdown(holiday, days) {
   return `It's ${days} days until ${holiday}!`
 }
