49e980691b6ee965bc56b88d393eac9640b1f3ca
Sarah Tennis <sarah.n.tennis@gmail.com>
2017-02-11 02:23:59 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// write your code below!
+// return string "Happy holidays!"
+function happyHolidays() {
+  return "Happy holidays!"
+}
+
+// accept parameter 'name', return string "Happy Holidays, ${name}!"
+function happyHolidaysTo(name) {
+  return `Happy holidays, ${name}!`
+}
+
+// accept two parameters 'holiday' and 'name'
+// return "Happy ${holiday}, ${name}!"
+function happyHolidayTo(holiday, name) {
+  return `Happy ${holiday}, ${name}!`
+}
+
+// accept two parameters 'days' and 'holiday'
+// return "It's ${days} days until ${holiday}!"
+function holidayCountdown(holiday, days) {
+  return `It's ${days} days until ${holiday}!`
+}
