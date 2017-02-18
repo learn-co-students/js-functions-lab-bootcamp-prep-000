18017be31a8478f4f40868b2da22cbf90b341e82
Paul Brown <paabrown@gmail.com>
2017-02-18 22:58:57 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // write your code below!
+function happyHolidays() {
+  return "Happy holidays!"
+}
+
+function happyHolidaysTo(name) {
+  if (name === undefined) {
+    return "Happy holidays, you!"
+  } else {
+  return `Happy holidays, ${name}!`
+}
+}
+
+function happyHolidayTo(holiday, name) {
+  if (name === undefined || holiday === undefined) {
+    return "Whatever dude"
+  } else {
+    return `Happy ${holiday}, ${name}!`
+  }
+}
+
+function holidayCountdown(holiday, days) {
+  if (holiday === undefined || days === undefined) {
+    return "Whatever dude"
+  } else {
+    return `It's ${days} days until ${holiday}!`
+  }
+}
