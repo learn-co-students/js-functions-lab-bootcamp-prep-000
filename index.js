8b5df375b0eb4e185c0bf558259e38e212f6f9b6
Monika Williams <monika.m.williams@gmail.com>
2017-02-21 15:34:28 +0000

    Done.

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 function happyHolidays(x) {
   return ("Happy holidays!");
 }
-function happyHolidaysTo(name) {
-  return  ("Happy holidays" +", "+ name +"!");
-}
+function happyHolidaysTo(name)
+ { return(`Happy holidays, ${name}!`);
+ }
 
 function happyHolidayTo(holiday, name) {
-  return ("Happy "+ holiday + ", "+ name + "!");
+  return (`Happy ${holiday}, ${name}!`);
 }
 
 function holidayCountdown(holiday, days) {
-  return ('It\'s ' + days + " days until " + holiday +"!");
+  return (`It\'s ${days} days until ${holiday}!`);
 }
