fb0be190a097c0a675b1c683c3625c9a2ed7b18c
Robert Williams <robert.j.williams.1823@gmail.com>
2017-02-24 22:52:07 +0000

    Done.

--- index.js
+++ index.js
@@ -4,6 +4,13 @@ function happyHolidays(string) {
 }
 
 function happyHolidaysTo(name) {
-  var name = you
-    return ('Happy holidays, ${name}!')
+    return (`Happy holidays, ${name}!`)
+}
+
+function happyHolidayTo(holiday, name) {
+    return (`Happy ${holiday}, ${name}!`)
+}
+
+function holidayCountdown(holiday, days) {
+    return (`It\'s ${days} days until ${holiday}!`)
 }
