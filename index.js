114fc84c43637b7d07b3dd4dab5649f540a840a8
Jess Hedges <jesskhedges@icloud.com>
2017-03-05 23:47:31 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // write your code below!
+function happyHolidays(sting){
+  return "Happy holidays!"
+}
+
+function happyHolidaysTo(name) {
+  var name = 'you'; {
+  return "Happy holidays, you!";
+}
+}
+
+function happyHolidayTo(holiday, name) {
+  var holiday = 'Independence Day';
+  var name = 'you'; {
+  return "Happy Independence Day, you!"
+}
+}
+
+function holidayCountdown(holiday, days) {
+  var holiday = 'Independence Day';
+  var days = '20'; {
+  return "It\'s 20 days until Mother\'s Day!"
+}
+}
