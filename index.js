608ea1a7d55728305b3e9eb80265593e66eb9290
Will Sauer <will@heueso.com>
2017-03-07 18:07:43 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// write your code below!
+//--------------------------------
+function happyHolidays ()
+{
+  return "Happy holidays!"
+}// write your code below!
+
+//--------------------------------
+function happyHolidaysTo(string)
+{
+  return `Happy holidays, ${string}!`
+}
+
+//--------------------------------
+function happyHolidayTo (holiday, name)
+{
+  return `Happy ${holiday}, ${name}!`
+}
+
+//--------------------------------
+function holidayCountdown (holiday,days)
+{
+  return `It's ${days} days until ${holiday}!`
+}
