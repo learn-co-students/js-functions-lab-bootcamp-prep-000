c932c48642bfa1615b5e0df9ed1b1e3c6819a7bd
Amadou Jallow <wuri1@live.com>
2017-03-09 06:49:14 +0000

    Done.

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 function happyHolidays(){
-  return "Happy holidays!"
+  return "Happy holidays!";
 }
 function happyHolidaysTo(name){
- var  name = Amadou
-  return "Happy holidays, ${name}!"
+  return "Happy holidays, you!";
+}
+function happyHolidayTo(holiday, name){
+  return "Happy Independence Day, you!";
+}
+function holidayCountdown(holiday, days){
+  return "It's 20 days until Mother's Day!";
 }
