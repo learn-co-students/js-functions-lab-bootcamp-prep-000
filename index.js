bd8760f8f14d44b5bbb347c4570201b4aa30170f
Lara Ismael <laraismael123@gmail.com>
2017-03-11 03:00:13 +0000

    Done.

--- index.js
+++ index.js
@@ -4,11 +4,11 @@ function happyHolidays() {
 }
 
 function happyHolidaysTo(name) {
-  return `Happy holidays, ${name}!`;
+  return 'Happy holidays, ' + name + '!';
 }
 
-function happyHolidaysTo(holiday, name) {
-  return `Happy ${holiday}, ${name}!`;
+function happyHolidayTo(holiday, name) {
+  return 'Happy ' + holiday + ', ' + name + '!';
 }
 
 function holidayCountdown(holiday, n) {
