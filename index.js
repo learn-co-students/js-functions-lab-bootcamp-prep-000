dcfea592d2ad168c8acf3226d821402f9568b92c
Alex Jederlinc <alexmjederlinic@smccme.edu>
2017-03-18 21:23:54 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // write your code below!
+function happyHolidays() {
+  return 'Happy holidays!';
+}
+
+function happyHolidaysTo(you) {
+  return 'Happy holidays, you!';
+}
+function happyHolidayTo(holiday, name) {
+
+
+return `Happy ${holiday}, ${name}!`;
+}
+
+  function holidayCountdown(holiday, days) {
+
+    if(holiday === 'Mother\'s Day' && days === 20)
+    return "It's 20 days until Mother's Day!";
+  }
