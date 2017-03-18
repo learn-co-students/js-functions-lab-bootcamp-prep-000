ca7c06ce01e81fddb4e8fc032ac12888373ba1c9
Catherine Espinosa <cat.espinosa@gmail.com>
2017-03-18 17:49:55 +0000

    Done.

--- index.js
+++ index.js
@@ -3,6 +3,14 @@ function happyHolidays () {
   return ("Happy holidays!");
 }
 
-happyHolidaysTo (name) {
-  return ("Happy holidays, ${name}!");
+function happyHolidaysTo(name) {
+  return `Happy holidays, ${name}!`
+}
+
+function happyHolidayTo(holiday,name) {
+  return `Happy ${holiday}, ${name}!`
+}
+
+function holidayCountdown(holiday,days){
+  return `It's ${days} days until ${holiday}!`
 }
