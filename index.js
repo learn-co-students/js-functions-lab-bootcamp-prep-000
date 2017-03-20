751d5281e85c2aa135664333bfee0db85bb70a8c
Rick Love <ricklove@outlook.com>
2017-03-20 21:36:48 +0000

    Done.

--- index.js
+++ index.js
@@ -9,5 +9,5 @@ function happyHolidayTo(holiday, name){
   return ('Happy Independence Day, you!')
 }
 function holidayCountdown(holiday, days){
-  return ("It's 20 days until Mother's Day!")
+  return ("It\'s 20 days until Mother's Day!")
 }
