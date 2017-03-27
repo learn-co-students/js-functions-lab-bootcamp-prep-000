74c750a7ce2ae362cf4e2478ef1558bf792fc697
SHASHA QIN <qin.shasha36@gmail.com>
2017-03-27 20:47:06 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,46 @@
-// write your code below!
+function happyHolidays(){
+  return "Happy holidays!";
+}// write your code below!
+
+function happyHolidaysTo (name) {
+  return `Happy holidays, ${name}!`;
+}
+
+function happyHolidayTo (holiday, name) {
+  return `Happy ${holiday}, ${name}!`;
+}
+
+function holidayCountdown (holiday, days) {
+  return `It's ${days} days until ${holiday}!`;
+}
+
+// function happyHolidaysTo (name){
+//   return 'Happy holidays, ${name}!';
+// }// wr
+// 
+// 
+// function happyHolidaysTo (holiday, name){
+//   return 'Happy ${holiday}, ${name}!';
+// }// wr
+// 
+// function holidayCountdown (holiday, days){
+//   return 'It\'s ${days} days until ${holiday}!';
+// }// wr
+
+
+// function happyHolidays(){
+//   return "Happy holidays!";
+// }// write your code below!
+// 
+// function happyHolidaysTo (name){
+//   return "Happy holidays, "+ name +"!";
+// }// wr
+// 
+// 
+// function happyHolidaysTo (holiday, name){
+//   return "Happy " + ${holiday} + ","+ ${name} +"!";
+// }// wr
+// 
+// function holidayCountdown (holiday, days){
+//   return "It\'s " + ${days} + "days until " + ${holiday} +"!";
+// }// wr
