3a8b5474d6d964f13206afcb6d46b96d050109ff
viba viba <vibasilly@gmail.com>
2017-03-30 21:21:03 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // write your code below!
+function happyHolidays() {
+  return "Happy holidays!" ;
+}
+happyHolidays()
+
+function happyHolidaysTo(name){
+ return "Happy holidays, " + 'you!';
+ }
+ happyHolidaysTo()
+
+ function happyHolidayTo(holiday,name) {
+   return "Happy"  + " Independence Day" + " you!" ;
+ }
+ happyHolidayTo()
+
+function holidayCountdown(number,holiday) {
+  return "It\'s, + 20" days" + " until Mother\s Day!" ;
+}
+holidayCountdown()
