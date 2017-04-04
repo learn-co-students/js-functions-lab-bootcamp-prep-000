5568b84b62ceb5b67170804a8b064701d839662c
Mandar Gade <mandargade.2997@gmail.com>
2017-04-04 22:50:22 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // write your code below!
+function happyHolidays(){
+  return 'Happy holidays!';
+}
+
+function happyHolidaysTo(name){
+  var string="Happy holidays, "+name+"!";
+  return string;
+}
+
+function happyHolidayTo(holiday,name){
+
+  return "Happy "+holiday+", "+name+"!";
+}
+
+function holidayCountdown(days,holiday){
+
+  return "It's "+holiday+" days until "+days+"!";
+}
