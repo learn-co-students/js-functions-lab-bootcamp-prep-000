156da2cfe0a39d06658ba7b1398d0c8209c4f450
Erik Ifland <ifland.erik@gmail.com>
2017-04-07 21:55:19 +0000

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,15 @@
 function happyHolidays(){
   return "Happy holidays!"
 }
+
+function happyHolidaysTo(string){
+  return "Happy holidays, " + string + "!"
+}
+
+function happyHolidayTo (string1,string2){
+  return "Happy " + string1 + ", " + string2 +"!"
+}
+
+function holidayCountdown (string, int){
+  return "It's " + int + "days until " + string "!"
+}
