ff5f2ef85010879580291ef21875110cf1faf9d4
Gabriele Hayden <gabriele.hayden@gmail.com>
2017-04-10 02:01:30 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,66 @@
 // write your code below!
+// write your code below!
+
+function happyHolidays() {
+    return "Happy holidays!";
+}
+
+function happyHolidaysTo(name) {
+    return `Happy holidays, ${name}!`;
+}
+
+function happyHolidayTo(holiday, name) {
+    return `Happy ${holiday}, ${name}!`;
+}
+
+function holidayCountdown(holiday, days) {
+    return `It\'s ${days} days until ${holiday}!`;
+}
+
+/*var name = "you";
+var days = "20";
+var holiday = "Independence Day";
+
+function happyHolidays() {
+    return 'Happy holidays!';
+};
+
+function happyHolidaysTo(name) {
+    return 'Happy holidays, ${name}!';
+}
+
+function happyHolidayTo(holiday, name) {
+    return 'Happy ${holiday}, ${name}!';
+}
+
+var holiday = "Mother's Day";
+
+"use strict";
+function holidayCountdown(days, holiday) {
+    return 'It\'s ${days} days until ${holiday}!';
+}
+
+/*
+var name = "you"
+var holiday = "Mother's Day"
+var days = "20"
+var holiday = "Independence Day"
+
+function happyHolidays() {
+  return "Happy holidays!"
+}
+
+function happyHolidaysTo(name) {
+  return "Happy holidays, ${name}!"
+}
+
+"use strict";
+function happyHolidayTo(holiday, name) {
+    return "Happy ${holiday}, ${name}!";
+}
+
+"use strict";
+function holidayCountdown(days, holiday) {
+    return "It's ${days} days until ${holiday}!";
+}
+*/
