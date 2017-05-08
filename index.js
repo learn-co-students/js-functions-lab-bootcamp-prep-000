f1f2764e8b038da53077860037b0f1cc356d470c
Todd Ciccarelli <todd.ciccarelli@gmail.com>
2017-05-08 05:40:40 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // write your code below!
+function happyHolidays() {
+  console.log('Happy Holidays!')
+  return ("Happy holidays!")
+}
+function happyHolidaysTo(name) {
+    return "Happy holidays, you!"
+}
+function happyHolidayTo(Christmas, Todd) {
+  return "Happy Independence Day, you!"
+}
+function holidayCountdown(holiday, days) {
+  return "It\'s 20 days until Mother\'s Day!"
+}
