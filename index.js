0996f4b55e684601b12c223dc216d36693feeaaa
Rachel Mandelbaum <rlmandelbaum@gmail.com>
2017-05-10 22:04:51 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // write your code below!
+function addTwoNumbers(numberOne, numberTwo){
+  return numberOne + numberTwo
+}
+
+function alwaysAddSameTwoNums(){
+  return 10000 + 250000
+}
+
+function happyHolidays(){
+return ("Happy holidays!")
+}
+
+function happyHolidaysTo(name){
+  return `Happy holidays, ${name}!`
+}
+
+function happyHolidayTo(holiday, name){
+  return `Happy ${holiday}, ${name}!`
+
+}
+
+function holidayCountdown(days, holiday){
+  return "It\'s 20 days until Mother\'s Day!"
+}
