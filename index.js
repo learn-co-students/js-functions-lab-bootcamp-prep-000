7228f77c3d62b00677190f793f00bcc3fc991932
Sean Clifford <sclifford6262@gmail.com>
2017-05-25 21:45:14 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // write your code below!
+var happyHolidays = function(){
+  return "Happy Holidays!";
+}
+
+var happyHolidaysTo = function(name){
+  return `Happy Holidays ${name}`;
+}
+
+var happyHolidayTo = function(name, holiday){
+  return 'Happy ${holiday}, ${name}'
+}
