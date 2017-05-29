9105502f23275ea9b175bca4a9c869cc960dacbd
Wayne Buxton <waynebuxton2184@yahoo.com>
2017-05-29 01:01:59 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // write your code below!
+function happyHolidays() {}
+// function someFn(){
+//   // logic goes inside the opening { and closing }
+//   // when the function is called then all the logic inside the curly brackets will run
+// }
+function happyHolidays() {return "Happy holidays!"}
+// function sayHappyHolidays(){
+//   return 'Happy holidays!'
+// }
+//
+// this defines the function sayHappyHoldays;
+//
+// then later on anytime we wanted to use that function
+//
+// console.log(sayHappyHolidays()); << this would log to the console `'Happy holidays!'` from that function ca
+function happyHolidaysTo(name) {return `Happy holidays, ${name}!`}
+function happyHolidayTo(holiday, name) {return `Happy ${holiday}, ${name}!`}
+function holidayCountdown(holiday,days) {return `It\'s ${days} days until ${holiday}!`}
