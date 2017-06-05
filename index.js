fd3ff734eb4828a9c2c6313b28971dc4d2983a67
Poke Sam <pokesam10031@yahoo.com>
2017-06-05 09:28:47 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // write your code below!
+function happyHolidays(string) {
+  return ("Happy holidays!")
+}
+
+function happyHolidaysTo(holiday, name) {
+  var name = "sam"
+  var holiday = "holidays"
+  return ("Happy ${holiday}, ${name}!")
+}
