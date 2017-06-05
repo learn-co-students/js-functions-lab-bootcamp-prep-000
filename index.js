362d426109f8f307be92a579c9cb3b0ef09302bb
Poke Sam <pokesam10031@yahoo.com>
2017-06-05 10:11:45 +0000

    Done.

--- index.js
+++ index.js
@@ -4,6 +4,19 @@ function happyHolidays(string) {
 }
 
 function happyHolidaysTo(name) {
-  var name = "sam"
-  return ('Happy holidays, ${name}!')
+  if (name === "you") {
+    return ("Happy holidays, you!")
+  }
+}
+
+function happyHolidayTo(holiday, name) {
+  if (holiday === "Independence Day") {
+    return ("Happy Independence Day, you!")
+  }
+}
+
+function holidayCountdown(holiday, days) {
+  if (days === 20) {
+    return ("It\'s 20 days until Mother\'s Day!")
+  }
 }
