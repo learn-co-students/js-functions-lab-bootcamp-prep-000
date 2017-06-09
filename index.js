28271269a8cdfbb17e02f2b3723329f3a367220c
Omari Blockton <oblockton@gmail.com>
2017-06-09 09:09:22 +0000

    Complete and Fix Errors

--- index.js
+++ index.js
@@ -3,5 +3,13 @@ function happyHolidays (){
 }// write your code below
 
 function happyHolidaysTo(name) {
-  return "Happy holidays, ${name}!"
+  return `Happy holidays, ${name}!`
+}
+
+function happyHolidayTo(holiday, name) {
+  return `Happy ${holiday}, ${name}!`
+}
+
+function holidayCountdown(holiday, days) {
+  return `It\'s ${days} days until ${holiday}!`
 }
