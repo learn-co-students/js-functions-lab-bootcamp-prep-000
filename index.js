60be318aa5fc9755d16accaa662527d1776e4814
Anthony Arce <axa2@cornell.edu>
2017-06-09 06:39:40 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// write your code below!
+var name = "you"
+var holiday = "Independence Day"
+var days = 20
+
+
+function happyHolidays(){
+  console.log('"Happy Holidays!"')
+}
+
+function happyHolidaysTo(name){
+  console.log('"Happy holidays ${name}!"')
+}
+
+function happyHolidayTo(holiday,name){
+  console.log('"Happy ${holiday}, ${name}!"')
+}
+
+function holidayCountdown(days,holiday){
+console.log('"It\'s ${days} days until ${holiday}!"')
+}
