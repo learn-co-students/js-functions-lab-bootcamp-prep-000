e190ff03a552fc64a5d611e0ac4990f4fcce8957
Anthony Arce <axa2@cornell.edu>
2017-06-09 07:17:39 +0000

    Done.

--- index.js
+++ index.js
@@ -1,18 +1,15 @@
-
-
-
 function happyHolidays(){
-  console.log(''Happy Holidays!'')
+  return 'Happy holidays!'
 }
 
 function happyHolidaysTo(name){
-  console.log(''Happy holidays ${name}!'')
+  return `Happy holidays, ${name}!`
 }
 
 function happyHolidayTo(holiday,name){
-  console.log(''Happy ${holiday}, ${name}!'')
+  return  `Happy ${holiday}, ${name}!`
 }
 
-function holidayCountdown(days,holiday){
-  console.log(''It\'s ${days} days until ${holiday}!'')
+function holidayCountdown(holiday,days){
+  return `It\'s ${days} days until ${holiday}!`
 }
