5946233dbe06edebab8a62bed551a52908587d0a
Ellyza Bachtiar <farah.e.bachtiar.effendi@vanderbilt.edu>
2017-06-11 14:36:37 +0000

    Done.

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ function happyHolidaysTo(name) {
   return (`Happy holidays, ${name}!`)
 }
 
-function happyHolidayTo(name) {
+function happyHolidayTo(holiday,name){
   return `Happy ${holiday}, ${name}!`
 }
 
