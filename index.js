540dc340a936752e69e53b54ed020faf7cf607b0
Will Middlebrook <will.d.middlebrook@gmail.com>
2017-06-14 22:21:32 +0000

    Done.

--- index.js
+++ index.js
@@ -6,17 +6,16 @@ return "Happy holidays!"
 
 function happyHolidaysTo (name)
 {
-return 'Happy holidays, you!'
+return `Happy holidays, ${name}!`
 }
 
 
 function happyHolidayTo (holiday, name)
 {
-  return "Happy ${holiday}, ${name}!"
+  return `Happy ${holiday}, ${name}!`
 }
 
-function holidayCountdown (days, holiday)
+function holidayCountdown (holiday, days)
 {
-  return "It's ${days} days until ${holiday}"
+  return `It's ${days} days until ${holiday}!`
 }
-happyHolidaysTo ("Independence Day", "you")
