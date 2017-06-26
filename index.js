40461221fe8cd2d1d4e03affccc7805acff74e24
Timothy Macrae <tmacrae93@gmail.com>
2017-06-26 02:01:13 +0000

    Done.

--- index.js
+++ index.js
@@ -2,6 +2,15 @@
 function happyHolidays(){
   return 'Happy holidays!'
 }
-function happyHolidayTo(name){
-  return 'Happy holidays,' + name'!'
+
+function happyHolidaysTo(name){
+  return 'Happy holidays, ' + name + '!';
+}
+
+function happyHolidayTo(holiday, name){
+  return 'Happy ' + holiday + ', ' + name + '!';
+}
+
+function holidayCountdown(holiday, days){
+  return "It's " + days + ' days until ' + holiday + '!';
 }
