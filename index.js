011f8f4cd51ddf613088194adee23bdec77cfbf8
Tzvi Hamerman <16guitar@gmail.com>
2017-06-26 22:28:41 +0000

    Done.

--- index.js
+++ index.js
@@ -2,17 +2,17 @@
 // parameters, the holiday name and number of days till that holiday. The function
 // should return the string "It's ${days} days until ${holiday}!"
 function happyHolidays() {
-  return "Happy holidays!";
+  return "Happy holidays!"
 }
 
 function happyHolidaysTo(name) {
-  return `Happy holidays, ${name}!`;
+  return `Happy holidays, ${name}!`
 }
 
 function happyHolidayTo(holiday, name) {
-  return `Happy ${holiday}, ${name}!`;
+  return `Happy ${holiday}, ${name}!`
 }
 
 function holidayCountdown(holiday, days) {
-  return `It's ${days} days until ${holiday}!`;
+  return `It's ${days} days until ${holiday}!`
 }
