9271ea95efeb0bb3a87c9f79e639e5b7cfa3167b
Kristen Peck <kpeck71@gmail.com>
2017-06-29 23:55:54 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// write your code below!
+function happyHolidays() {
+  return('Happy holidays!')
+};
+
+function happyHolidaysTo() {
+  var name = "Kristen";
+  return ("Happy holidays, you!")
+};
+
+function happyHolidayTo(name,holiday) {
+  var name = "you";
+  var holiday = "Independence Day";
+  return("Happy " + holiday + ", " + name+ "!")
+};
+
+function holidayCountdown(holiday,days) {
+  var holiday = "Mother's Day!";
+  var days = 20;
+  return("It's " + days + " days until " + holiday)
+}
