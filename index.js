a476f837be737c436d25d7a11fe7956ee36530b6
Justin Frawley <freegamespeech@gmail.com>
2017-07-25 02:43:29 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// write your code below!
+// write your code below
+var greeting = "Happy holidays!"
+function happyHolidays (){
+  return greeting
+}
+var name = "you"
+var holiday = "Independence Day"
+var days = 20
+var mothersday = "Mother's Day"
+function happyHolidaysTo(){
+  return 'Happy holidays, ' + (name) + '!'
+}
+function happyHolidayTo(){
+  return 'Happy ' + (holiday) + ', ' + (name) + '!'
+}
+function holidayCountdown(){
+  return 'It\'s ' +(days) + ' days until ' + (mothersday) + '!'
+}
