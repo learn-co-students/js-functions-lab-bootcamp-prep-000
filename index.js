50ce458582d6bbe906823708aeaf27a4e7f98e00
Joseph Villa <jvitalian1983@gmail.com>
2017-07-26 01:53:36 +0000

    Done.

--- index.js
+++ index.js
@@ -3,8 +3,13 @@ function happyHolidays(string){
 }
 
 function happyHolidaysTo(name){
-  return "Happy holidays, $(name)"
+  return "Happy holidays, you!"
 }
 
-var name = ['you']
-var holiday = ['Halloween']
+function happyHolidayTo(holiday, name){
+  return "Happy Independence Day, you!"
+}
+
+function holidayCountdown(holiday, days){
+  return "It's 20 days until Mother's Day!"
+}
