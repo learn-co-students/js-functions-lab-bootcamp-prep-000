2bd5810c572812fd1923b8a66ac8673bfe647261
Joseph Villa <jvitalian1983@gmail.com>
2017-07-26 01:43:54 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-// write your code below!
+function happyHolidays(string){
+  return "Happy holidays!"
+}
+
+function happyHolidaysTo(name){
+  return "Happy holidays, $(name)"
+}
+
+var name = ['you']
+var holiday = ['Halloween']
