7e4bdb68238ea48b9ae53a791906d79d62d13127
Gevanstron <gen.athena.evans@gmail.com>
2017-07-28 04:03:01 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // write your code below!
+var happy = "Happy holidays!"
+function happyHolidays(string) {
+  return happy
+}
+var name = "Genevieve"
+function happyHolidaysTo(name) {
+  return (`Happy holidays, ${name}!`)
+}
+var holiday = "Christmas"
+function happyHolidayTo(holiday, name) {
+  return (`Happy ${holiday}, ${name}!`)
+}
+var days = 12
+function holidayCountdown(holiday, days) {
+  return (`It's ${days} days until ${holiday}!`)
+}
