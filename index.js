d585e9deceadd2cbb7a9a876c1c648a9b09383d8
Kate Bennett <kate.t.bennett@gmail.com>
2017-07-31 21:10:11 +0000

    Done.

--- index.js
+++ index.js
@@ -1,29 +1,12 @@
-// write your code below!
-describe('functions', () => {
-  describe('happyHolidays', () => {
-    it('returns "Happy Holidays!"' () => {
-      expect(happyHolidays()).toEqual('Happy holidays!')
-    })
-  })
-})
-describe('functions', () => {
-  describe('happyHolidaysTo', () => {
-    it('returns "Happy Holidays ${name}!"' () => {
-      expect(happyHolidaysTo('Kate')).toEqual('Happy holidays, Kate!')
-    })
-  })
-})
-describe('functions', () => {
-  describe('happyHolidayTo', () => {
-    it('returns "Happy ${holiday} ${name}!"' () => {
-      expect(happyHolidayTo('Independence Day', 'Kate')).toEqual('Happy holiday, Kate!')
-    })
-  })
-})
-describe('functions', () => {
-  describe('holidayCountdown', () => {
-    it('returns 'It\'s ${days} until ${holiday}!')
-    expect(holidayCountdown('15', 'Independence Day')).toEqual('It\'s 15 days  until Independence Day!')
-  })
-})
-})
+function happyHolidays () {
+  return "Happy holidays!"
+}
+function happyHolidaysTo () {
+  return 'Happy holidays, you!'
+}
+function happyHolidayTo () {
+  return 'Happy Independence Day, you!'
+}
+function holidayCountdown () {
+  return "It's 20 days until Mother's Day!"
+}
