440e7526af448927c89393391276a916dda5a8b9
Christine <christinetran825@gmail.com>
2017-08-04 00:19:42 +0000

    Done.

--- index.js
+++ index.js
@@ -4,17 +4,13 @@ function happyHolidays() {
 }
 
 function happyHolidaysTo(name) {
-  var name = "you"
   return `Happy holidays, ${name}!`
 }
 
 function happyHolidayTo(holiday,name) {
-  var holiday = "Independence Day"
-  var name = "you"
   return `Happy ${holiday}, ${name}!`
 }
 
 function holidayCountdown(holiday,days) {
-  var days = "20"
   return `It\'s ${days} days until ${holiday}!`
 }
