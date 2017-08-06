b763b83dc30d9effcea7ea3ae43b221bacab97af
deeptilokhande <deepti.lokhande@gmail.com>
2017-08-06 05:48:29 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // write your code below!
+function happyHolidays(){
+  return"Happy Holidays!"
+}
+console.log(happyHolidays());
+
+function happyHolidaysTo(inname){
+ return`Happy Holidays ${inname}`
+}
+
+console.log(happyHolidaysTo('Deepti'));
+function happyHolidaysTo(festival,name){
+  return`Happy ${festival}, dear ${name}`
+}
+console.log(happyHolidaysTo('RakshaBandhan','Swara'));
+function holidayCountdown(days,festival){
+  return `Its ${days} days till ${festival}`
+}
+console.log(holidayCountdown(7,'Ganesh Chaturthi'));
