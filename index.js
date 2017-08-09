948ae510a3f47144ca2b5037e6dbb61c46c86773
Jordan Appel <appel.jaa@gmail.com>
2017-08-09 04:43:17 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // write your code below!
+function happyHolidays(string){
+  return "Happy holidays!"
+}
+function happyHolidaysTo(string){
+  return "Happy holidays, ${name}!"
+}
+function happyHolidayTo(string){
+  return "Happy ${holiday}, ${name}!"
+}
