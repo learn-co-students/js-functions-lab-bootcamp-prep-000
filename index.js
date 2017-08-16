6b89cea07b832e2f3c3c38107722ded19721afad
Kevin Nguyen <ktn555@tamu.edu>
2017-08-16 01:39:51 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// write your code below!
+// writefunction happyHolidays()
+
+function happyHolidays() {
+  return "Happy holidays!"
+}
+function happyHolidaysTo(string){
+  return `Happy holidays, ${string}!`
+}
+
+function happyHolidayTo(holiday,name) {
+  return `Happy ${holiday}, ${name}!`
+}
+
+function holidayCountdown(holiday, days){
+  return `It's ${days} days until ${holiday}!`
+}
