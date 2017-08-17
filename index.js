a05fb6eef67604ce6f7cfc92344ad1c2030b4e37
aglassgalaxy <aglassgalaxy@gmail.com>
2017-08-17 14:51:06 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// write your code below!
+  function shout(string) {
+    return string.toUpperCase()
+  }
+  function whisper(string) {
+    return string.toLowerCase()
+  }
+  function logShout(string)  {
+    return string.toUpperCase
+  }
+  function logWhisper(string)  {
+    return string.toLowerCase
+  }
+  function sayHiToGrandma(string)  {{
+    if ("Hi Grandma!")
+    if (string==stringto.upperCase)
+    {
+      return "YES INDEED!"
+    }
+    else
+  {
+    return "I CAN'T HEAR YOU!"
+  }
+  function ILoveYouGrandma(string)  {{
+  if (string="I love you Grandma"))
+  {
+    return string("I love you, too.")
+  }}}
