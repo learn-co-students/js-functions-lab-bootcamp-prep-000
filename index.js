d249e04e5d610778b48d7bc8ff778b124ced2ef7
David Crowell <davidleecrowell@gmail.com>
2017-08-18 23:41:54 +0000

    Done.

--- index.js
+++ index.js
@@ -2,12 +2,19 @@
 function happyHolidays () {
   return "Happy holidays!"
 }
+happyHolidays()
+
 function happyHolidaysTo(name) {
   return "Happy holidays, ${name}!"
 }
+happyHolidaysTo("David")
+
 function happyHolidayTo(holiday, name) {
   return "Happy ${holiday}, ${name}! "
 }
+happyHolidayTo("Christmas", "David")
+
 function holidayCountdown(days, holiday) {
   return "It's $[days] days until ${holiday}!"
 }
+holidayCountdown("9", "Christmas")
