c42e67c936b4fa40e2a27cb80f546292c7bae48c
KevinUndaho <mariahmurray5@gmail.com>
2017-08-22 17:50:30 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // write your code below!
+function happyHolidays () {
+  var greeting = "Happy holidays!"
+  return greeting
+
+}
+
+console.log(happyHolidays())
+
+function happyHolidaysTo (name) {
+  var greeting = "Happy holidays, " + name +"!"
+  return greeting
+
+}
+
+console.log(happyHolidaysTo("Bob"))
+
+function happyHolidayTo (holiday, name) {
+  var greeting = "Happy " + holiday + ", " + name + "!"
+  return greeting
+
+}
+
+console.log(happyHolidayTo("Passover", "Bob"))
+
+function holidayCountdown(holiday, days) {
+  var greeting = "It's " + days + " days until " + holiday + "!"
+  return greeting
+
+}
+
+console.log(holidayCountdown("3", "Gvanim"))
