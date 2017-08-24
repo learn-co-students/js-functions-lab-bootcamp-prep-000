294fa24dda791280f554b143980b23b572cee904
Freddy Hernandez <freddy.hernandez@fandm.edu>
2017-08-24 16:45:57 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // write your code below!
+function happyHolidays() {
+  return 'Happy holidays!'
+}
+function happyHolidaysTo(Rosie) {
+  return `Happy holidays, ${Rosie}!`
+}
+function happyHolidaysTo(birthday, Rosie) {
+  return `Happy ${birthday}, ${Rosie}!`
+}
