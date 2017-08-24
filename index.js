a61d299f7bdf5fc23da6f35fbac1d6e97bf73903
William Birkett <williebirkett@gmail.com>
2017-08-24 21:52:29 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// write your code below!
+var name = "Willie";
+var holiday = "Christmas";
+var days = "249";
+
+function happyHolidays() {
+  return `Happy holidays!`;
+}
+
+function happyHolidaysTo(name) {
+  return `Happy holidays, ${name}!`;
+}
+
+function happyHolidayTo(holiday, name) {
+  return `Happy ${holiday}, ${name}!`;
+}
+
+function holidayCountdown(holiday, days) {
+  return `It\'s ${days} days until ${holiday}!`
+}
