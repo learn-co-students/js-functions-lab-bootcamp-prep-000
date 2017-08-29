dfe017c393fe50410f277019fdae91f2522ad978
Nemanja Drakulovic <drdrakula96@hotmail.com>
2017-08-29 02:49:24 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// write your code below!
+function happyHolidays(){
+  console.log("Happy Holidays!")
+}
+
+function happyHolidaysTo(name){
+  console.log("Happy holidays,  " + name)
+}
+
+function happyHolidayTo(holiday,name){
+  console.log('Happy ' + holiday + ', ' + name + '!')
+}
+
+happyHolidays()
+
+happyHolidaysTo("Nemanja")
+
+happyHolidayTo("easter","Nemanja")
