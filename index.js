f3650b6611da07c6d4e3963c1312fa83b7bb938c
Paul Cotter <pcotter85@gmail.com>
2017-08-30 22:55:09 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // write your code below!
+function happyHolidays () {
+  return "Happy holidays!"
+}
+
+function happyHolidaysTo(name) {
+  return `Happy holidays, ${name}!`;
+}
+
+function happyHolidayTo(holiday, name) {
+  return `Happy ${holiday}, ${name}!`
+}
+
+function holidayCountdown(holiday, days) {
+    if (days > 1) {
+      return `It's ${days} days until ${holiday}!`;
+    } else if (days === 1) {
+      return `${holiday} is tomorrow!`;
+    } else if (days === 0) {
+      if (holiday === "Christmas") {
+        return `Merry ${holiday}!`;
+      } else {
+       return `Happy ${holiday}!`;
+       }
+     }
+}
