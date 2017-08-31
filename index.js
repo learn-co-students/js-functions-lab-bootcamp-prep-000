c8ff0b78a9415f68ba20de4869ef3b871124f63a
mstmari <michael.landrum.st.marie@gmail.com>
2017-08-31 04:25:00 +0000

    Done.

--- index.js
+++ index.js
@@ -5,5 +5,10 @@ return ('Happy holidays!')
 function happyHolidaysTo(name) {
   return `Happy holidays, ${name}!`
 }
-function happyHolidaysTo(holiday, name) {
+function happyHolidayTo(holiday, name) {
   return `Happy ${holiday}, ${name}!`
+}
+
+  function holidayCountdown(holiday, days) {
+    return `It's ${days} days until ${holiday}!`
+  }
