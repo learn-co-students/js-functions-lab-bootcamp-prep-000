523e361f178d339ab1864fc095047db231f96ab3
Brandon Brasson <rocksong622@gmail.com>
2017-09-04 01:42:55 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-// write your code below!
+function happyHolidays() {
+}
+function happyHolidays() {
+   return "Happy holidays!!";
+}
+
+ 'Hello!'.toUpperCase() // 'HELLO!'
+
+ function happyHolidaysTo(string) {
+   return string.toUpperCase()
+ }
+
+ function happyHolidaysTo(string) {
+ }
+
+
+
+    'HELLO'.toLowerCase()
+
+    function happyHolidaysTo(string) {
+   return `Happy holidays, ${name}!`
+ }
+
+ function happyHolidaysTo(string) {
+    return `Happy ${holiday}, ${name}!`
+
+
+    function holidayCountdown(string) {
+       return `It's ${days} days until ${holiday}!`
