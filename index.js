58a8ddac79c54dc7fa482cac290b1b1b48b6895a
Adiel Wilson <adielwilson@gmail.com>
2017-09-05 00:16:43 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
- function happyHolidays() {  return "Happy holidays!";};
- function happyHolidaysTo(name) {  return `Happy holidays, ${name}!`;};
- function happyHolidayTo(holiday, name) {  return `Happy ${holiday}, ${name}!`;};
- function holidayCountdown(holiday, number) {  return `It\'s ${number} days until ${holiday}!`;};
+// write your code below!
+function happyHolidays(){ return 'Happy holidays!';};
+function happyHolidaysTo(name) {  return `Happy holidays, ${name}!`;};
+function happyHolidayTo(holiday, name) {  return `Happy ${holiday}, ${name}!`;};
+function holidayCountdown(holiday, number) {  return `It\'s ${number} days until ${holiday}!`;};
