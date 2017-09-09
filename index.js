a0ec02fec74e44915b0d4ce59659956ccfc46b65
Armelle Rose-Olesco <armelleroseolesco@gmail.com>
2017-09-09 04:54:43 +0000

    Done.

--- index.js
+++ index.js
@@ -5,3 +5,11 @@ function happyHolidays() {
 function happyHolidaysTo(name) {
   return `Happy holidays, ${name}!`
 }
+
+function happyHolidayTo(holiday, name) {
+  return `Happy ${holiday}, ${name}!`
+}
+
+function holidayCountdown(holiday, days) {
+  return `It's ${days} days until ${holiday}!`
+}
