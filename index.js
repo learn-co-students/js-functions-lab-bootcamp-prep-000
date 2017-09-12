56eb07b6328cf5431abd247261a9b120156f0cc1
rsmile74 <smithrds74@hotmail.com>
2017-09-12 19:39:22 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// write your code below!
+function happyholidays(string){
+  return 'Happy Holidays'
+}// write your code below!
+function happyholidays(rhonda){
+  console.log('Happy Holidays ${Rhonda}!')
+}
+function happyHolidaysto(){
+  return 'Happy ${holiday}, ${name!}'
+}
+function holidayCountdown() {
+  return 'It;s ${days} days until ${holiday}!'
+}
