76a2df3ad00749394f2860f3d0b62a6ef77e5546
JCS <joshsad2@gmail.com>
2017-09-12 06:31:28 +0000

    Done.

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 // write your code below!
 function happyHolidays() {
-  return "Happy Holidays!";
+  return "Happy holidays!";
 }
 
 function happyHolidaysTo(name) {
-  return `Happy holidays ${name}`;
+  return `Happy holidays, ${name}!`;
 }
 
 function happyHolidayTo(holiday, name) {
