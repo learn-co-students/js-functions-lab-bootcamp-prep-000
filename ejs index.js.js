3e5eb5b23addf76159fb8eee970ad8be094627ae
rsmile74 <smithrds74@hotmail.com>
2017-09-12 19:52:00 +0000

    Done.

--- ejs index.js.js
+++ ejs index.js.js
@@ -2,11 +2,11 @@ function happyholidays(){
   return "Happy Holidays!"
 }// write your code below!
 function happyHolidaysTo(name){
-  return 'Happy Holidays, ${name}!'
+  return 'Happy Holidays To ${name}!')
 }
 function happyHolidaysto(holiday, name){
   return 'Happy ${holiday}, ${name!}'
 }
 function holidayCountdown(holiday, days) {
-  return 'It\'s ${days} days until ${holiday}!'
+  return 'It;s ${days} days until ${holiday}!'
 }
