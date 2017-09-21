b8781b4fbb1e069ac00d536af484037e210ad559
Brandon Brasson <rocksong622@gmail.com>
2017-09-21 18:19:30 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
 // write your code below!
+console.log("Hello, World!")
+
+var name = "Joe"
+var height = 20
+var message = `${name} is ${height} inches tall`
+
+module.exports = {name, height, message}
+
+
+Function happyHolidays () {
+return ("Happy holidays!")
+}
+Function happyHolidaysTo(name) {
+return `Happy holidays, ${name}!`
+}
+function happyHolidaysTo (holiday,name) {
+return `${Happy holidays}, ${name}!`
+}
+function holidayCountdown (holiday,days) {
+ return`It's ${days} days until ${holiday}!`
+}
+happyHolidayTo('Happy holidays, you!')
+happyHolidayTo('Independence Day', 'you')
+holidayCountdown("Mother's Day", 20)
+
+function happyHolidaysTo(name) -> String {
+    return "Happy holidays!"
+}
+
+function happyHolidaysTo (holiday,name) -> String {
+    return "Happy holidays, ${name}!"
+}
+function happyHolidaysTo(name: String, holiday,days) -> String {
+return`Happy holidays, ${name}!`
+  }
+  function sayHello() {
+    console.log('Hello!')
+  }
+
+  console.log (message)
+
+console.log (message)
