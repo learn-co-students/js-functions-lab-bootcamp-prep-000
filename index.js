d727852d3a27cb536913c3d01e1664b862a2354e
Tracy Fernandez <tr.ferna@gmail.com>
2017-09-26 18:21:54 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// write your code below!
+var holiday = Halloween
+var name = Susan
+var days = "30"
+
+function happyHolidays() {
+  console.log("Happy Holidays!")
+  return "Happy Holidays!"
+}
+function happyHolidaysTo("name") {
+  return `Happy Holidays, ${name}!`
+}
+function happyHolidayTo("holiday", "name") {
+  console.log("Happy Halloween, Susan!")
+  return `Happy ${holiday}, ${name}`
+}
+function holidayCountdown("holiday", "days") {
+  return `It's ${days} days until ${holiday}!`
+}
