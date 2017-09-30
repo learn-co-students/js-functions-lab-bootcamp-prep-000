d199c30836aaaef34f34dfe55356fbe59b0041fa
Tyler Wales <walestyl@gmail.com>
2017-09-30 21:53:19 +0000

    Done.

--- index.js
+++ index.js
@@ -12,6 +12,6 @@ function happyHolidayTo(holiday, name) {
   return `Happy ${holiday}, ${name}!`
 }
 
-function holidayCountdown(days, holiday) {
+function holidayCountdown(holiday, days) {
   return `It's ${days} days until ${holiday}!`
 }
