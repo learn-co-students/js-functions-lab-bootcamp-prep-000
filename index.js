97448babbfc824a2a5e99a9733e93a64d550596a
James Fernandez <jffernan@comcast.net>
2017-10-02 15:44:31 -0700

    Re-do & added pseudocode // comments

--- index.js
+++ index.js
@@ -1,16 +1,34 @@
-function happyHolidays () {
-  return "Happy holidays!"
+/* 
+1. Declare a function named happyHolidays
+2. When call function, return string "Happy holidays!".
+*/
+function happyHolidays() {
+  return "Happy holidays!";
   }
 
-function happyHolidaysTo (name) {
-  return "Happy holidays, you!"
+/* 
+1. Declare a function named happyHolidaysTo.
+2. Function accepts parameter named name, whose value is string.
+3. When call function, return the string `Happy holidays, ${name}!`.
+*/
+function happyHolidaysTo(name) {
+  return "Happy holidays, you!";
 }
 
-
-function happyHolidayTo (holiday, name) {
-  return "Happy Independence Day, you!"
+/* 
+1. Declare a function named happyHolidaysTo.
+2. Function accepts 2 parameters named holiday and name, whose values are strings.
+3. When call function, return string `Happy ${holiday}, ${name}!`.
+*/
+function happyHolidayTo(holiday, name) {
+  return "Happy Independence Day, you!;"
 }
 
-function holidayCountdown (days, holiday) {
-  return "It's 20 days until Mother's Day!"
+/* 
+1. Declare a function named holidayCountdown.
+2. Function accepts 2 parameters named days, holiday, whose values are a number and string. 
+3. When call function, return string `It's ${days} days until ${holiday}!`.
+*/
+function holidayCountdown(days, holiday) {
+  return `It is ${days} days until ${holiday}!`;
 }
