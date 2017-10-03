d023fe7cf1ee4c2f9727d6492ea9274bc4559e69
asha muttinakntimath <asha.mumath@gmail.com>
2017-10-03 17:57:13 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // write your code below!
+function happyHolidays(name)
+{
+  console.log(`happyHolidays,${name}`)
+
+}
+happyHolidays('asha')
+function happyHolidaysTo(holiday,name)
+{
+  console.log(`happy,${holiday},{$name}`)
+}
+happyHolidaysTo(10,'akshara')
