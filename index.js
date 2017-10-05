e5f5dc2ee6b375a030523bbc4b900d2bad303bba
geoffcraz <geoffcraz@gmail.com>
2017-10-05 16:50:16 +0000

    Done.

--- index.js
+++ index.js
@@ -11,5 +11,5 @@ function happyHolidayTo(holiday, name) {
 }
 
 function holidayCountdown(holiday, days) {
-  return `It's ${20} days until ${holiday}!`
+  return `It's ${days} days until ${holiday}!`
 }
