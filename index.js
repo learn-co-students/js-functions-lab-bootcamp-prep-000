70325335be36793f4212afd108f540daa26ed9a0
pirushanp <pirushanp@gmail.com>
2017-10-16 20:27:00 +0000

    Done.

--- index.js
+++ index.js
@@ -1,22 +1,16 @@
 // write your code below!
 function happyHolidays() {
-  var happyHolidays= "Happy holidays!"
   return "Happy holidays!"
 }
+
 function happyHolidaysTo(name) {
-  var happyHolidaysTo = "Happy holidays, ${name}!"
-  var name = "Pirushan"
-  return "Happy holidays, ${name}!"
+  return `Happy holidays, ${name}!`
 }
+
 function happyHolidayTo(holiday, name) {
-   var happyHolidayTo = "Happy ${holiday}, ${name}!"
-   var holiday = "Happy Independence Day"
-   var name = "you"
-  return "Happy ${holiday}, ${name}!"
+  return `Happy ${holiday}, ${name}!`
 }
+
 function holidayCountdown(holiday, days) {
-  var holidayCountdown = "It\'s ${days} days until ${holiday}!"
-  var holiday = "Mother's Day"
-  var days = 20
-  return "It\'s ${days} days until ${holiday}!"
+  return `It's ${days} days until ${holiday}!`
 }
--- test/index-test.js
+++ test/index-test.js
@@ -9,19 +9,19 @@ describe('functions', () => {
 
   describe('happyHolidaysTo(name)', () => {
     it('returns "Happy holidays, ${name}!"', () => {
-      expect(happyHolidaysTo('name')).toEqual("Happy holidays, ${name}!")
+      expect(happyHolidaysTo('you')).toEqual('Happy holidays, you!')
     })
   })
 
   describe('happyHolidayTo(holiday, name)', () => {
     it('returns "Happy ${holiday}, ${name}!"', () => {
-      expect(happyHolidayTo("independance day, you")).toEqual("Happy ${holiday}, ${name}!")
+      expect(happyHolidayTo('Independence Day', 'you')).toEqual('Happy Independence Day, you!')
     })
   })
 
   describe('holidayCountdown(holiday, days)', () => {
     it('returns "It\'s ${days} days until ${holiday}!"', () => {
-      expect(holidayCountdown("Mother's Day", 20)).toEqual("It\'s ${days} days until ${holiday}!")
+      expect(holidayCountdown("Mother's Day", 20)).toEqual("It's 20 days until Mother's Day!")
     })
   })
 })
