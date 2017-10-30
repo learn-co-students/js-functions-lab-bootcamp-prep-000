72459bbb05b4cba1532ec2a4f6b6cd1838eee0b9
Andrew McDermitt <andrew.mcdermitt@doitbest.com>
2017-10-30 19:27:05 +0000

    Done.

--- index.js
+++ index.js
@@ -11,6 +11,6 @@ function happyHolidayTo(holiday, name){
   return `Happy ${holiday}, ${name}!`
 }
 
-holidayCountdown(holiday, days){
+function holidayCountdown(holiday, days){
   return `It's ${days} days until ${holiday}!`
 }
