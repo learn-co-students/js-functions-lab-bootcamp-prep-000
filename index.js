666bdcbaa5983ebba2ad97de35315d1e348c8a23
Kent Picou <kpicou@wesleyan.edu>
2017-10-30 22:10:53 +0000

    Done.

--- index.js
+++ index.js
@@ -2,3 +2,11 @@
 function happyholidays(){
   return "Happy holidays!"
 }
+
+function happyHolidaysto(holiday, name){
+  return `Happy ${holiday}, ${name}!`
+}
+
+function holidayCountdown(days, holiday){
+  return `It's ${days} days until ${holiday}!`
+}
