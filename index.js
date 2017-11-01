5a4710a32cb387ce7c474d40e9c87710dd9716bd
Laura Travin <laura.travin@gmail.com>
2017-11-01 08:16:15 +0000

    Done.

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // write your code below!
 function happyHolidays(){
-  console.log("Happy holidays!")
+  console.log(`Happy holidays!`)
 }
 function happyHolidaysTo(name){
   console.log(`Happy holidays, ${name}!`)
