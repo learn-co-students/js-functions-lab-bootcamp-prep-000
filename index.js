ea178d700d2637e028c864ec5977ffd5dc036f27
Jessica Potter <jessica.potter@state.co.us>
2017-11-03 18:00:39 +0000

    Done.

--- index.js
+++ index.js
@@ -1,18 +1,22 @@
 // write your code below!
 function happyHolidays () {
-  return "Happy Holidays"
+    console.log("Happy holidays!")
+    return "Happy holidays!"
 }
 
 function happyHolidaysTo (name) {
-  return 'Happy holidays, ${name}!'
+  return `Happy holidays, ${name}!`
 }
 
 function happyHolidayTo (holiday, name) {
-  return 'Happy ${holiday}, ${name}!'
+  return `Happy ${holiday}, ${name}!`
 }
 
-function holidayCountdown (days, holiday) {
-  return 'It's ${days} days until ${holiday}!''
+function holidayCountdown (holiday, days) {
+  return `It's ${days} days until ${holiday}!`
 }
 
-happyHolidays
+ happyHolidays ()
+ happyHolidaysTo ()
+ happyHolidayTo ()
+ holidayCountdown ()
