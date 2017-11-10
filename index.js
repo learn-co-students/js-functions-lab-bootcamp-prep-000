54f1afb7fca2fa549ef93f0666ba9d37972907ad
Yufeng Liu Reach <yufeng8@vt.edu>
2017-11-10 17:24:47 +0000

    Done.

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
 // write your code below!
 function happyHolidays() {
-  return "Happy holidays!"
+  return 'Happy holidays!'
 }
 
 function happyHolidaysTo(name) {
-  return 'Happy holidays, ${name}!'
+  return 'Happy holidays, ' + name + '!'
 }
 
 function happyHolidayTo(holiday, name) {
-  return 'Happy ${holiday}, ${name}!'
+  return 'Happy ' + holiday + ', ' + name + '!'
 }
 
-function holidayCountdown(days, holiday) {
-  return 'Its ${days} days until ${holiday}!'
+function holidayCountdown(holiday, days) {
+  return `It's ${days} days until ${holiday}!`
 }
