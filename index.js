3a40b1d1fdcb21fc631f29c1ff27f7354d8c559f
Veronica Kim <vero.a.kim@gmail.com>
2017-11-14 23:28:02 +0000

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
 function happyHolidays() {
-  return ("Happy Holidays")
+  return ("Happy holidays!");
+}
+
+
+function happyHolidaysTo(name) {
+  if (name === `Happy holidays, ${name}!`);
+    return 'Happy holidays, you!'
+}
+
+function happyHolidayTo(holiday, name) {
+  return `Happy ${holiday}, ${name}!`
+}
+
+function holidayCountdown(holiday, days) {
+  return `It\'s ${days} days until ${holiday}!`
 }
