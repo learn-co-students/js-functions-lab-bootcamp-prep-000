4435d870404eb363f7f20da855ced3eeddc1ad37
Alejandro Madrigal <madrigalalex88@mail.fresnostate.edu>
2017-11-29 06:58:43 +0000

    Done.

--- index.js
+++ index.js
@@ -12,10 +12,10 @@ function happyHolidayTo(holiday, name) {
 }
 
 function holidayCountdown(days, holiday){
-  return (`It\'s ${days} days until ${holiday}!`)
+  return ("It's" + " " +  holiday + " "+ "days until" + " "+ days +"!")
 }
 
 happyHolidays();
 happyHolidaysTo("you");
 happyHolidayTo("thanksgiving", "alex");
-holidayCountdown(20, "Mother\'s Day");
+holidayCountdown(20,"Mother's Day");
