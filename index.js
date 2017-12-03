9879d1018dc0a32578b0d051eb710bb091ab8b6a
xielucy513 <xie.lucy513@gmail.com>
2017-12-03 18:02:21 +0000

    Done.

--- index.js
+++ index.js
@@ -2,3 +2,11 @@
 function happyHolidays () {
   return "Happy holidays!"
 }
+
+function happyHolidaysTo (name) {
+  return `Happy holidays, ${name}!`
+}
+
+function happyHolidayTo (holiday, name) {
+  return `Happy ${holiday}, ${name}!`
+}
