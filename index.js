40dcdab428c44c19fcb99b445ea6e8413de71a16
Stephanie <s.a.james5@gmail.com>
2017-12-07 18:26:37 +0000

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
-function happyHolidays(greeting) {return "Happy holidays!"}
-function happyHolidaysTo(you) {return 'Happy holidays, you!'}
-function happyHolidaysTo(Independence Day, you) {return 'Happy Independence Day, you!'}
+function happyHolidays(greeting) {return `Happy holidays!`}
+function happyHolidaysTo(you) {return `Happy holidays, you!`}
+function happyHolidayTo(IndependenceDay, you) {return `Happy Independence Day, you!`}
+function holidayCountdown(MothersDay, twenty) {return `It's 20 days until Mother's Day!`}
