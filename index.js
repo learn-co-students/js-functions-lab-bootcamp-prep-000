8307fb1862f7e3feae63965325c447eb26c689bf
Max Kelly <maxleekelly@gmail.com>
2017-12-11 20:03:19 +0000

    Done.

--- index.js
+++ index.js
@@ -9,5 +9,5 @@ function happyHolidayTo(holiday, name) {
   return `Happy ${holiday}, ${name}!`;
 }
 function holidayCountdown(holiday, days) {
-  return `It's ${days} days until ${holiday}`;
+  return `It's ${days} days until ${holiday}!`;
 }
