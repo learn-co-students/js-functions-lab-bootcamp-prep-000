29239aa41635a3c53f466c509c09904db45fe812
Radha Patel <radhapatel99@gmail.com>
2017-12-12 02:52:18 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // write your code below!
+function happyHolidays(){console.log('Happy holidays!')}
+happyHolidays()
+
+
+function happyHolidaysTo(name){console.log(`Happy Holidays, ${name}!`)}
+
+happyHolidaysTo('Radha')
+
+function happyHolidayTo(holiday, name){console.log(`Happy ${holiday}, ${name}!`)}
+happyHolidayTo("diwali", "Radha")
+
+function holidayCountdown (days, holiday){console.log(`It's ${days} days until ${holiday}!`)}
+holidayCountdown ("11", "Diwali")
