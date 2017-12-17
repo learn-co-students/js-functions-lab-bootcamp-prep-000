3a52c9e87cbb1ba4c8ece9f91e091f1e3b1db922
Van A. Dao <daoanhvan81@gmail.com>
2017-12-17 13:02:39 +0000

    Done.

--- index.js
+++ index.js
@@ -4,18 +4,18 @@ function happyHolidays() {
 }
 
 function happyHolidaysTo(name) {
-  return 'Happy holidays, ${name}!';
+  return 'Happy holidays, ' + name + '!';
 }
 
 function happyHolidayTo(holiday, name) {
-  return 'Happy ${holiday}, ${name}!';
+  return ('Happy ' + holiday + ', ' + name + '!');
 }
 
 function holidayCountdown(holidayName, numOfDaysLeft) {
-  return 'It\'s ${numOfDaysLeft} days until ${holidayName}!';
+  return 'It\'s ' + numOfDaysLeft + ' days until ' + holidayName +'!';
 }
 
 happyHolidays();
 happyHolidaysTo('you');
 happyHolidayTo('Independence Day', 'you');
-holidayCountdown('Mother\'s Day', 20);
+holidayCountdown('Mother\'s Day', 20); 
