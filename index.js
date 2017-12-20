849ea9f6f8f4a7f093b13c44f488750568ee9852
Steven Harms <steven.harms@flatironschool.com>
2017-12-20 23:18:56 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-// write your code below!
+var happyHolidays = () => "Happy holidays!";;
+
+var happyHolidaysTo = (name) => `Happy holidays, ${name}!`;
+
+var happyHolidayTo = (holiday, name) => `Happy ${holiday}, ${name}!`;
+
+var holidayCountdown = (holiday, days)  => `It's ${days} days until ${holiday}!`
