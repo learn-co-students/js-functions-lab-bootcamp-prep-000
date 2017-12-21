4073cc851f00c59e899d473cea660549b6e0d615
Edward Norton <edwardnorton@icloud.com>
2017-12-21 03:18:45 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // write your code below!
+function happyHolidays() {
+  return "Happy holidays!"
+}
+
+function happyHolidaysTo(name) {
+  console.log(`Happy holidays, ${name}!`);
+  return "Happy holidays, you!";
+}
+
+happyHolidaysTo('Katie') // "Happy holidays, Katie!"
+
+function happyHolidayTo(holiday, name) {
+  console.log(`Happy ${holiday}, ${name}!`)
+  return "Happy Independence Day, you!"
+}
+
+function holidayCountdown(days, holiday) {
+  console.log(`It's ${days} days until ${holiday}!`)
+  return "It\'s 20 days until Mother\'s Day!"
+}
