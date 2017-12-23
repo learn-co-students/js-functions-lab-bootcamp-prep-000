51c279975d4f31b9e87862a68819955a5f09908f
Joseph Epifano <joepifano@gmail.com>
2017-12-23 02:45:45 +0000

    Done.

--- index.js
+++ index.js
@@ -1,4 +1,16 @@
-
-function happyHolidays.()
-  return 
-function 'Happy holidays, ${name}!'
+function happyHolidays() {
+   return "Happy Holidays!";
+ }
+ 
+ function happyHolidaysWithName(name){
+   return "Happy Holidays, " + ${name}
+ }
+ 
+ function happyAnyHoliday(holiday, name){
+   return "Happy " + holiday + " " + name;
+ }
+ 
+ 
+ function holidayCountDown(holiday, days){
+   return "It's " + days + " days till " + holiday;
+ }
