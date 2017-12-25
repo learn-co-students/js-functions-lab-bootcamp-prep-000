16af1ff70f2c87f6c41c5084961fa895accf67ed
asmaa.abbas@wework.com <asmaa.abbas@wework.com>
2017-12-25 16:31:18 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // write your code below!
+
+function happyHolidays() {
+  return `Happy holidays!`;
+}
+
+// const happyHolidays = () => {
+//   return `Happy holidays!`;
+// };
+
+function happyHolidaysTo (name) {
+  return `Happy holidays, ${name}!`;
+}
+
+// const happyHolidaysTo = (name) => {
+//   return `Happy holidays, ${name}!`;
+// };
+
+function happyHolidayTo (holiday, name) {
+  return `Happy ${holiday}, ${name}!`;
+}
+
+// const happyHolidayTo = (holiday, name) => {
+//   return `Happy ${holiday}, ${name}!`;
+// };
+
+function holidayCountdown (holiday, numOfDays) {
+  return `It's ${numOfDays} days until ${holiday}!`;
+}
+
+// const holidayCountdown = (holiday, numOfDays) => {
+//   return `It's ${numOfDays} days until ${holiday}`;
+// };
