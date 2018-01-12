03c262ab40bcd5da00bc779cdf807e78376371ad
kendocode <git@kendocoding.com>
2018-01-12 02:59:41 -0600

    Fix typo

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ function happyHolidaysTo(name) {
     return `Happy holidays, ${name}!`;
 }
 
-function happyHolidayto(holiday, name) {
+function happyHolidayTo(holiday, name) {
     return `Happy ${holiday}, ${name}`;
 }
 
