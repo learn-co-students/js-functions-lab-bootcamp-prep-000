bf0adeb2c36381284ae7e65fc528d231747cff24
Omar Ch <mhcomar28@gmail.com>
2018-01-12 20:37:47 +0000

    Done.

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ function happyHolidays() {
   return "Happy holidays!";
 }
 
-function happyHolidaysTo(you) {
-  return `Happy holidays, ${you}!`;
+function happyHolidaysTo(karen) {
+  return `Happy holidays, ${karen}!`;
 }
 
 function happyHolidayTo(holiday, name) {
@@ -14,3 +14,6 @@ function happyHolidayTo(holiday, name) {
 function holidayCountdown(holiday, days) {
   return `It's ${days} days until ${holiday}!`
 }
+
+//Lab didn't specify to make changes the index-test.js.
+//tests won't pass unless 100% exact.
