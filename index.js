ece7c83c2ca977c3c0d7690149d89c040f0dfd6b
Omar Ch <mhcomar28@gmail.com>
2018-01-12 20:05:08 +0000

    Done.

--- index.js
+++ index.js
@@ -1,17 +1,19 @@
 // write your code below!
-
-function happyHolidays() {
-  return "Happy holidays!";
+function happyHolydays(){
+  return "Happy holidays!"
 }
 
-function happyHolidaysTo(name) {
-  return `"Happy holidays, ${name}!"`;
+function happyHolidaysTo(karen) {
+  return `"Happy holidays, ${karen}!"`;
 }
 
 function happyHolidayTo(easter, karen) {
-  return `Happy ${easter}, ${karen}!`;
+  return `"Happy ${easter}, ${karen}!"`;
 }
 
 function holidayCountdown(32, Valentines) {
-  return `It's ${32} days until ${Valentines}!`;
+  return `"It's ${32} days until ${Valentines}!"`;
 }
+
+//I believe this is the right code, However I keep getting Reference errors
+//even after cloned the lab, and started fresh.
