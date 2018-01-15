9e74158fa480acad30eab268d2672861d28e7b6a
Iho Somnam <ihosomnam4741@gmail.com>
2018-01-15 11:03:27 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // write your code below!
+var n1 = "Happy holidays!"
+var n2 = `Happy holidays, ${name}!`
+var n3 = `Happy ${holiday}, ${name}!`
+var n4 = `It's ${days} days until ${holiday}!`
+
+function happyHolidays {
+  console.log("Happy holidays!");
+}
+function happyHolidaysTo {
+  console.log(n2);
+}
+function happyHolidayTo {
+  console.log(n3);
+}
+function holidayCountdown {
+  console.log(n4);
+}
