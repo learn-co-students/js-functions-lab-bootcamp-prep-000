baf2ac848288815b8b50edb50cbc2189c02fd1e5
Dana Kessler <kessler.danam@gmail.com>
2018-01-16 03:04:13 +0000

    Done.

--- index.js
+++ index.js
@@ -1,16 +1,24 @@
 // write your code below!
+var days = 20;
+var name = `you`;
+
 function happyHolidays() {
   return "Happy holidays!";
 }
 
 function happyHolidaysTo(name) {
-  return 'Happy holidays, ${name}!';
+  console.log(happyHolidaysTo);
+  return `Happy holidays, ${name}!`;
 }
 
 function happyHolidayTo(holiday, name) {
-  return 'Happy ${holiday}, ${name}!';
+  console.log(happyHolidayTo);
+  return `Happy ${holiday}, ${name}!`;
+  var holiday = `Independence Day`;
 }
 
 function holidayCountdown (holiday, days) {
-  return 'It's ${days} days until ${holiday}!';
+  console.log(holidayCountdown);
+  return `It's ${days} days until ${holiday}!`;
+  var holiday = `Mother's Day`;
 }
