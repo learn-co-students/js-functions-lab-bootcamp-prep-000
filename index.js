9b79c234f4e00c47e241bedf8821c09c221a9acf
Brian Ringley <brian.ringley@wework.com>
2018-01-24 23:15:26 +0000

    Done.

--- index.js
+++ index.js
@@ -4,13 +4,18 @@ function happyHolidays(){
 }
 
 function happyHolidaysTo(name){
-  return 'Happy holidays, ${name}!'
+  return `Happy holidays, ${name}!`
 }
 
-function happyHolidayTo(name, holiday){
- return 'Happy ${holiday}, ${name}!' 
+function happyHolidayTo(holiday, name){
+ return `Happy ${holiday}, ${name}!`
 }
 
 function holidayCountdown(days, holiday){
-  return 'It\'s ${days} days until ${holiday}!'
+  return `It\'s ${days} days until ${holiday}!`
 }
+
+console.log(happyHolidays())
+console.log(happyHolidaysTo('you'))
+console.log(happyHolidayTo('you', 'Independence Day'))
+console.log(holidayCountdown(20, 'Mother\'s Day'))
