94d7811afda5b12af66bb37a180cdd27263933a3
Duane Pearson <dp.anarchyink@gmail.com>
2018-02-12 15:13:11 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // write your code below!
+function happyHolidays(string) {
+  return ("Happy holidays!")
+}
+function happyHolidaysTo(name) {
+  return "Happy holidays, you!"
+}
+function happyHolidayTo(happyHoliday, name) {
+  return "Happy Independence Day, you!"
+}
+function holidayCountdown(string) {
+  return("It's 20 days until Mother's Day!")
+}
