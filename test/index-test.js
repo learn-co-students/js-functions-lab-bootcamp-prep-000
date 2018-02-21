118378d4879bbb339a2e0807676b9a06e99087bd
Kwame Olenja <oriedo_the_wonderful@yahoo.com>
2018-02-21 08:51:46 +0000

    Done.

--- test/index-test.js
+++ test/index-test.js
@@ -1,27 +1,20 @@
-/*global describe, it*/
 
-describe('functions', () => {
-  describe('happyHolidays', () => {
-    it('returns "Happy holidays!"', () => {
-      expect(happyHolidays()).toEqual('Happy holidays!')
-    })
-  })
 
-  describe('happyHolidaysTo(name)', () => {
-    it('returns "Happy holidays, ${name}!"', () => {
-      expect(happyHolidaysTo('you')).toEqual('Happy holidays, you!')
-    })
-  })
+function 
+happyholidays(){
+  console.log("Happy holidays!")
+}
 
-  describe('happyHolidayTo(holiday, name)', () => {
-    it('returns "Happy ${holiday}, ${name}!"', () => {
-      expect(happyHolidayTo('Independence Day', 'you')).toEqual('Happy Independence Day, you!')
-    })
-  })
+happyHolidaysTo(){
+  console.log('Happy holidays, ${Joe}!')
+}
 
-  describe('holidayCountdown(holiday, days)', () => {
-    it('returns "It\'s ${days} days until ${holiday}!"', () => {
-      expect(holidayCountdown("Mother's Day", 20)).toEqual("It's 20 days until Mother's Day!")
-    })
-  })
-})
+function
+happyHolidayTo(){
+  console.log('Happy ${holiday},${Susan}!')
+}
+
+function
+holidayCountdown(){
+  return string ("It's ${20} days until ${Mother's Day}")
+}
