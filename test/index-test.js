7d13a4971598b5f26dda849409464705e2309690
George Valdes <gv2186@gmail.com>
2018-02-26 17:40:52 +0000

    Done.

--- test/index-test.js
+++ test/index-test.js
@@ -1,27 +1,27 @@
 /*global describe, it*/
 
-describe('functions', () => {
+describe('function', () => {
   describe('happyHolidays', () => {
     it('returns "Happy holidays!"', () => {
-      expect(happyHolidays()).toEqual('Happy holidays!')
-    })
-  })
+      expect(happyHolidays()).toEqual('Happy holidays!');
+    });
+  });
 
   describe('happyHolidaysTo(name)', () => {
     it('returns "Happy holidays, ${name}!"', () => {
-      expect(happyHolidaysTo('you')).toEqual('Happy holidays, you!')
-    })
-  })
+      expect(happyHolidaysTo(name)).toEqual('Happy holidays, ${name}');
+    });
+  });
 
   describe('happyHolidayTo(holiday, name)', () => {
     it('returns "Happy ${holiday}, ${name}!"', () => {
-      expect(happyHolidayTo('Independence Day', 'you')).toEqual('Happy Independence Day, you!')
-    })
-  })
+      expect(happyHolidayTo("Mother's Day", "Mom")).toEqual("Happy Mother's Day, Mom!");
+    });
+  });
 
   describe('holidayCountdown(holiday, days)', () => {
     it('returns "It\'s ${days} days until ${holiday}!"', () => {
-      expect(holidayCountdown("Mother's Day", 20)).toEqual("It's 20 days until Mother's Day!")
-    })
-  })
-})
+      expect(holidayCountdown("Mother's Day", 20)).toEqual("It's 20 days until Mother's Day!");
+    });
+  });
+});
