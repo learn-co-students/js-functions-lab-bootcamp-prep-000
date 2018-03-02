dbe7c94be7b8f60193a80f57ca6752c2f3785aa8
spyvsspy989@gmail.com <spyvsspy989@gmail.com>
2018-03-02 00:31:21 +0000

    Done.

--- test/index-test.js
+++ test/index-test.js
@@ -1,9 +1,17 @@
-Function ('happyHolidayTo')
-  returns ("Happy Holidays, ${name}!");
+Function ('happyHolidayTo') output ('var name')
+  if ('happyHolidayTo)' output
+('Happy Holidays, ${"var name");
+
 }
 Function ('happyHolidayTo')
   returns ("Happy Holidays, ${name}!");
 }
 Function ('holidayCountdown') {
   returns ('It's ${days} days until ${holiday}!');
+}
+
+Function ('happyHolidayTo')('holidayCountdown) output ('var name')('happyHolidayTo')('holidayCountdown'(
+  if ('happyHolidayTo)' output
+('Happy Holidays, ${"var name");
+else if ('holidayCountdown) output ('It's ${days} days until ${holiday}!)'
 }
