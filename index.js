311f244aba5e451380f2cf6d47033a47d2d21953
Jian Ting Li <jianting2103@yahoo.com>
2018-03-02 05:07:12 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // write your code below!
+function happyHolidays() {
+  return "Happy holidays!";
+}
+
+
+function happyHolidaysTo(name) {
+  return `Happy holidays, ${name}!`;
+}
+console.log(happyHolidaysTo('you'));
+
+
+function happyHolidayTo(holiday, name) {
+  return `Happy ${holiday}, ${name}!`;
+}
+
+
+function holidayCountdown(holiday, days) {
+  return `It's ${days} days until ${holiday}!`;
+}
+
+
+function say(greeting, firstName) {
+  return `${greeting}, ${firstName}!`
+}
+say("hello", 'jian ting')
