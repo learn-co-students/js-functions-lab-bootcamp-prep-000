a4a2fbe3423e4bf4fa11b9d033cdb504f56a8849
spyvsspy989@gmail.com <spyvsspy989@gmail.com>
2018-03-02 00:17:05 +0000

    Done.

--- test/index-test.js
+++ test/index-test.js
@@ -1,34 +1,9 @@
-/*global describe, it*/
-
-describe('functions', () => {
-  describe('happyHolidays', () => {
-    it('returns "Happy holidays!"', () => {
-      expect(happyHolidays()).toEqual('Happy holidays!')
-    })
-  })
-Function ('happyHolidays'){
-  returns = "Happy holidays";
-}
-
-  describe('happyHolidaysTo(name)', () => {
-    it('returns "Happy holidays, ${name}!"', () => {
-      expect(happyHolidaysTo('you')).toEqual('Happy holidays, you!')
-    })
-  })
 Function ('happyHolidayTo')
   returns ("Happy Holidays, ${name}!");
 }
-  describe('happyHolidayTo(holiday, name)', () => {
-    it('returns "Happy ${holiday}, ${name}!"', () => {
-      expect(happyHolidayTo('Independence Day', 'you')).toEqual('Happy Independence Day, you!')
-    })
-  })
+Function ('happyHolidayTo')
+  returns ("Happy Holidays, ${name}!");
+}
 Function ('holidayCountdown') {
   returns ('It's ${days} days until ${holiday}!');
 }
-  describe('holidayCountdown(holiday, days)', () => {
-    it('returns "It\'s ${days} days until ${holiday}!"', () => {
-      expect(holidayCountdown("Mother's Day", 20)).toEqual("It's 20 days until Mother's Day!")
-    })
-  })
-})
