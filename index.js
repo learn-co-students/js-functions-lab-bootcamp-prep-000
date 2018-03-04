2696277d8c4f92e83178639d16db221a2dc9c944
alfedd@crimson.ua.edu Fedd <alfedd@crimson.ua.edu>
2018-03-04 23:55:26 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // write your code below!
+var functionholiday; name
+
+function happyHolidays(string) {
+  return "Happy holidays!"
+}
+function happyHolidaysTo(string) {
+  return `Happy holidays, you!`
+}
+function happyHolidayTo(string) {
+  return "Happy Independence Day, you!"
+}
+function holidayCountdown(string) {
+  return "It's 20 days until Mother's Day!"
+}
