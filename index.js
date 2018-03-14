dc9baf683b8554969c34d475cecf7a5f7e290493
chewning.cl@gmail.com <chewning.cl@gmail.com>
2018-03-14 05:14:51 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // write your code below!
+
+
+// 1 
+
+function happyHolidays() {
+  return "Happy holidays!" 
+}
+
+function happyHolidaysTo(name) {
+  return "Happy holidays, ${name}!"  
+}
+
+ happyHolidayTo("Chris");
+
+
+function happyHolidaysTo(holiday, name) {
+  return 'Happy ${holiday}, ${name}!'
+}
+
+happyHolidaysTo("Easter, Chris");
+
+
+function holidayCountdown(days, holiday) {
+ return "It's ${days} days until ${holiday}!"   
+}
+
+holidayCountdown("18, Easter");
