67356e7af5625ef48cd4cfdadc97d1dd8ed51d2a
John Kalinauskas <jkalinauskas@live.com>
2018-03-18 21:54:49 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // write your code below!
+function happyHolidays () { 
+  return "Happy holidays!"
+}
+
+ function happyHolidaysTo(name) {
+  return `Happy holidays, ${name}!`
+} 
+ 
+ function happyHolidayTo(holiday , name) {
+ return `Happy ${holiday}, ${name}!`
+ }
+ 
+ function holidayCountdown(holiday, days) {
+   return `It's ${days} days until ${holiday}!`;
+ }
+  
+   
+   
+   
+ 
+  /*    
+  Write a function named holidayCountdown. This function should accept two parameters, the holiday name and number of days till that holiday. The function should return the string `It's ${days} days until ${holiday}!
+  
+  
+  
+  
+  
+  */
