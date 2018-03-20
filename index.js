d053069e593196ad19daa06e5cb34b9442b7fb18
wangfanqoo@126.com Wang <wangfanqoo@126.com>
2018-03-20 20:08:47 +0000

    Done.

--- index.js
+++ index.js
@@ -2,5 +2,12 @@
 function happyHolidays() {
   return "Happy holidays!"
 }
-
-happyHolidays()
+function happyHolidaysTo(name) {
+  return `Happy holidays, ${name}!`
+}
+function happyHolidayTo(holiday,name) {
+  return `Happy ${holiday}, ${name}!`
+}
+function holidayCountdown(holiday,days){
+  return `It's ${days} days until ${holiday}!`
+}
