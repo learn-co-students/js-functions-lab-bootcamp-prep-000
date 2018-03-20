e7d37ec14dbff8f884fdee85818d23dc54d2415e
Alex Edwards <info@acedigitalagency.net>
2018-03-20 16:13:26 +0000

    Done.

--- index.js
+++ index.js
@@ -3,11 +3,11 @@ function happyHolidays() {
  return "Happy holidays!";
 }
 function happyHolidaysTo(name) {
-  return 'Happy holidays, ${name}!';
+  return `Happy holidays, ${name}!`;
 }
-function happyHolidaysTo(holiday, name) {
-  return 'Happy ${holidays}, ${name}!';
+function happyHolidayTo(holiday, name) {
+  return `Happy ${holiday}, ${name}!`;
 }
 function holidayCountdown(days, holiday) {
-  return 'It\'s ${days} days until ${holiday}!';
+  return `It\'s ${holiday} days until ${days}!`;
 }
