258c1e445719fc89c7ca5ce400c3d184d66467c5
gustavo.malamud@gmail.com Malamud <gustavo.malamud@gmail.com>
2018-03-27 06:39:44 +0000

    Done.

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
 // write your code below!
 
 function happyHolidays() {
-  return("Happy Holidays!");
+  return "Happy holidays!";
 }
 
-function happyHolidaysTo(string) {
-  
+function happyHolidaysTo(name) {
+ return "Happy holidays, " + name + "!";
 }
+
+function happyHolidayTo(holiday, name) {
+  return `Happy ${holiday}, ${name}!`;
+}
+
+function holidayCountdown (holiday, days) {
+  return `It's ${days} days until ${holiday}!`;
+} 
--- test/root.js
+++
@@ -1,9 +0,0 @@
-global.expect = require('expect')
-
-const fs = require('fs')
-const jsdom = require('mocha-jsdom')
-const path = require('path')
-
-jsdom({
-  src: fs.readFileSync(path.resolve(__dirname, '..', 'index.js'), 'utf-8')
-})
