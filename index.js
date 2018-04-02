36784cbedd85373cd7c32b550b7f423dad162ce0
dervishiamela@gmail.com Dervishi <dervishiamela@gmail.com>
2018-04-02 21:24:44 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// write your code below!
+function happyHolidays() {
+return "Happy holidays!" 
+}
+function happyHolidayTo(holiday, name) {
+return 'Happy Independence Day, you!'
+}
+function happyHolidaysTo(name) {
+return 'Happy holidays, you!'
+}
+function holidayCountdown(holiday, days) {
+  return "It's 20 days until Mother's Day!"
+}
