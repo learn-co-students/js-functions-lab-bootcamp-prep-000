20983fa305c9d7057497da4176aa0cb9ee453e6c
Bethany Oestrich <bethany@oestrich.org>
2018-04-02 02:30:32 +0000

    Done.

--- index.js
+++ index.js
@@ -6,13 +6,13 @@ function happyHolidays ()
 
 function happyHolidaysTo(name)
 {
-  return "Happy holidays, you!"
+  return `Happy holidays, ${name}!`
 }
 function happyHolidayTo(holiday, name)
 {
-  return "Happy Independence Day, you!"
+  return `Happy ${holiday}, ${name}!`
 }
 function holidayCountdown(days,holiday)
 {
-  return "It's 20 days until Mother's Day!"
+  return `It's ${holiday} days until ${days}!`
 }
