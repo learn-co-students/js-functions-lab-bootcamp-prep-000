e0d2749e56b01376b04153f035054970ac50d55d
Kelly Rae Ramirez <k.rae1993@yahoo.com>
2018-04-11 21:41:01 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// write your code below!
+function happyHolidays() {
+  return "Happy holidays!"
+  console.log("Happy holidays!")
+}
+function happyHolidaysTo() {
+  return "Happy holidays, you!"
+  console.log("Happy holidays, you!")
+}
+function holidayCountdown() {
+  return "It's 20 days until Mother's Day!"
+  console.log("It's 20 days until Mother's Day!")
+}
+function happyHolidayTo() {
+  return "Happy Independence Day, you!"
+  console.log("Happy Independence Day, you!")
+}
