430089a3e9ce136e9a606105ffa464baea2e6c24
Daniel Mack <daniel.mack@powerhrg.com>
2018-04-11 00:37:33 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // write your code below!
+
+
+function happyHolidays(string) {
+  return "Happy holidays!"
+}
+
+function happyHolidaysTo(name) {
+  return `Happy holidays, ${name}!`
+}
+happyHolidaysTo(you)
+
+function happyHolidayTo(holiday, name) {
+  return `Happy ${holiday}, ${name}!`
+}
+happyHolidayTo(kwanzaa, daniel)
+
+function holidayCountdown(holiday, days) {
+  return `It's ${days} days until ${holiday}!`
+}
+holidayCountdown("Mother's Day", 20)
