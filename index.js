15df16ffdcf14730f02ac3025e7bf70edb467738
Adam Barton <adam.barton333@gmail.com>
2018-04-11 03:17:27 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // write your code below!
+function happyHolidays(String) {
+ return "Happy holidays!"
+}
+function happyHolidaysTo(string) {
+  return "Happy holidays, you!"
+}
+function happyHolidayTo(holiday, name) {
+ return "Happy Independence Day, you!"
+}
+function holidayCountdown(holiday, days) {
+  return "It\'s 20 days until Mother\'s Day!"
+}
