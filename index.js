0fe49d720947ca5f29a28c5b264a77e5a61d8b40
ochoa.kayla@yahoo.com <ochoa.kayla@yahoo.com>
2018-04-14 00:40:16 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // write your code below!
+function happyHolidays() {return "Happy holidays!"
+}
+
+function happyHolidaysTo(name) {            
+return `Happy holidays, ${name}!`
+}
+
+
+function happyHolidaysTo(holiday, name) {
+return `Happy ${holiday}, ${name}!`
+}
+
+
+function holidayCountdown(holiday, days) {
+  console.log(`It's ${days} days until ${holiday}!`)
+}
