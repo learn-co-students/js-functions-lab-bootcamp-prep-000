8fb7e04fbf03c5f8eacc521e9d4f60c0a6378408
yuxichen11@gmail.com <yuxichen11@gmail.com>
2018-04-15 03:52:39 +0000

    Done.

--- index.js
+++ index.js
@@ -8,6 +8,6 @@ function happyHolidaysTo(name) {
 function happyHolidayTo(holiday, name) {
   return `Happy ${holiday}, ${name}!`
 }
-function holidayCountdown(days, holiday) {
+function holidayCountdown(holiday, days) {
   return `It's ${days} days until ${holiday}!`
 }
