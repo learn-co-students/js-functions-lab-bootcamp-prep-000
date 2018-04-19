52e3e2a01fbd8f12624debd7ecefa2b456796df9
Cathy Lii <cathy.y.lii@gmail.com>
2018-04-19 17:24:22 +0000

    Done.

--- index.js
+++ index.js
@@ -4,15 +4,19 @@ return "Happy holidays!";
 }
 
 function happyHolidaysTo(name) {
-return 'Happy holidays, you!'; 
+return `Happy holidays, ${name}!`; 
 }
+
+happyHolidaysTo("you")
 
 function happyHolidayTo(holiday,name) {
-return 'Happy Independence Day, you!';
+return `Happy ${holiday}, ${name}!`;
 }
 
+happyHolidayTo("Independence Day", "you")
+
 function holidayCountdown(holiday,days) {
-return 'It\'s ${daysa} days until ${holiday}!';
+return `It\'s ${days} days until ${holiday}!`;
 }
 
 holidayCountdown("Mother's Day", "20");
