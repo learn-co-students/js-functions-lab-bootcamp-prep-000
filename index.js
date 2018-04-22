7556d019d2053e87503c2f0e3cc569a5a7fa2f7f
khadija farah <abdix129@umn.edu>
2018-04-22 21:53:48 +0000

    Done.

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
 // write your code below!
-function happyHolidays(string){
-  return string("Happy holidays"); 
+function happyHolidays(){
+  return "Happy holidays!"
+}
+function happyHolidaysTo(name) {
+  return "Happy holidays, you!"
+}
+function happyHolidayTo(holiday, name) {
+  return "Happy Independence Day, you!"
+}
+function holidayCountdown(holiday, days) {
+  return "It's 20 days until Mother's Day!"
 }
