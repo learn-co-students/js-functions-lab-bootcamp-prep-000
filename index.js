ba9544b45f6ff8495806f2b3b2181307e01e46ef
Rebecca Govert <becci.govert@gmail.com>
2018-04-24 16:11:49 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // write your code below!
+
+function happyHolidays() {
+  return("Happy holidays!")
+}
+
+function happyHolidaysTo(name){
+  return `Happy holidays, ${name}!`
+}
+
+function happyHolidayTo(holiday, name) {
+  return `Happy ${holiday}, ${name}!`
+}
+
+function holidayCountdown(holiday, days) {
+  return `It\'s ${days} days until ${holiday}!`
+}
+
+/* tried (days, holidays) but it needed to be in order (holiday, days). Must watch this order of what directions are asking. I though I needed days first because days came first in my return line. Not true. */
