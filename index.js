74342120027032e2e43ed12d645ac63276ffa176
Austin Curry <austin.curry360@gmail.com>
2018-04-28 00:11:20 +0000

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
 function holidayCountdown(thanksgiving,days) {
 return `It's 20 days until Mother's Day!`
 }
+function happyHolidayTo(thanksgiving, sister) {
+return `Happy Independence Day, you!`
+}
+function happyHolidaysTo(name) {
+return `Happy holidays, you!`
+}
+function happyHolidays() {
+return `Happy holidays!`
+}
