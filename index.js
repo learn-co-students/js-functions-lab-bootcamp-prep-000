d785468de81eda1bb512eb1e7a0e967fa5dbd01a
Daniel Hope <daniel@codewithhope.com>
2018-05-01 00:13:14 +0000

    Done.

--- index.js
+++ index.js
@@ -11,5 +11,5 @@ function happyHolidayTo(holiday, name) {
 }
 
 function holidayCountdown(holiday, days) {
-  return `It's ${days} days until ${holiday}`;
+  return `It's ${days} days until ${holiday}!`;
 }
