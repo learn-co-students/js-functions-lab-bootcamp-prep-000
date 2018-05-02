d02fba068799b3fecfaaae4369956df10ad4cf05
sedat_3278@hotmail.com <sedat_3278@hotmail.com>
2018-05-02 21:48:41 +0000

    Done.

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ function happyHolidaysTo(name){
 
 
 function happyHolidayTo(holiday, name){
-  return `Happy  ${holiday}, ${name}!`
+  return `Happy ${holiday}, ${name}!`
 }
 
 function holidayCountdown(holiday, days){
