3ad541457fcb39df8f81fac1b36186ea6a72ed82
Richi Dubey <herestobeingawesome@gmail.com>
2018-05-03 19:00:46 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // write your code below!
+function happyHolidays()
+{
+  const ret="Happy holidays!";
+  
+    return ret;
+  }
+  
+  
+function happyHolidaysTo(name)
+{
+  const ret="Happy holidays, "+name+"!";
+  
+    return ret;
+  }
+  
+  
+function happyHolidayTo(holidays,name)
+{
+  const ret="Happy "+holidays+", "+name+"!";
+  
+    return ret;
+  }
+
+  
+function holidayCountdown(holidays,num)
+{
+  const ret="It's "+num+" days until "+holidays+"!";
+  
+    return ret;
+  }
