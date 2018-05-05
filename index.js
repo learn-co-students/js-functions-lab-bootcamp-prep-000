8971e9c07de34ed5dc9a3831ffaacf77c135af88
Steven Crouse <sdcrouse91@gmail.com>
2018-05-05 07:10:22 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // write your code below!
+function happyHolidays() {
+  //console.log("Happy holidays!")
+  return "Happy holidays!"
+}
+
+function happyHolidaysTo(name) {
+ //console.log(`Happy holidays, ${name}!`)
+  return `Happy holidays, ${name}!`
+}
+
+function happyHolidayTo(holiday, name) {
+  //console.log(`Happy ${holiday}, ${name}!`)
+  //console.log("Merry Christmas, Santa! (And Happy Birthday, Jesus!)")
+  return `Happy ${holiday}, ${name}!`
+}
+
+/* A little scary that I'm writing this next function the week before Mother's Day, which just HAPPENS to be the holiday that it's being tested with! */
+
+function holidayCountdown(holiday, days) {
+  //console.log(`It's ${days} days until ${holiday}! (More like 8, actually...)`)
+  return `It's ${days} days until ${holiday}!`
+}
