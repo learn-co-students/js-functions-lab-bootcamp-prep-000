e59a5a97cabf89f385ed45d16127bd466112c95b
Victoria Hong <victoriaxhong@gmail.com>
2018-05-06 15:05:36 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // write your code below!
+function happyHolidays() { console.log('Happy holidays!') }
+
+function happyHolidaysTo(firstName) { console.log(`Hello, ${name}!` ) }
+
+
+function happyHolidayTo(holiday, firstName) { console.log(`${greeting}, ${firstName}!`) }
+
+function holidayCountdown (days, holiday) console.log(`It's ${days} days until ${holiday}!` ) { return `${days}, ${holiday}` }
