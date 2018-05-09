c1ece3db09670d3aa96457a4b743597720126451
An An <vantran.learning@gmail.com>
2018-05-09 02:56:47 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // write your code below!
+function happyHolidays(){
+  return "Happy holidays!"
+}
+
+function happyHolidaysTo(name){
+  return `Happy holidays, ${name}!`
+}
+
+function happyHolidayTo(holiday, name){
+  return `Happy ${holiday}, ${name}!`
+}
+
+function holidayCountdown(holiday, dayleft){
+  return `It's ${dayleft} days until ${holiday}!`
+}
+
+happyHolidays()
+happyHolidaysTo("Van")
+holidayCountdown("20","Mother's Day!")
