0759b8ccce85eda8ad91adccd5e874c0dbe4a39d
Romana Akter <urmee04sust@gmail.com>
2018-05-16 21:04:39 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // write your code below!
+function happyHolidays() {
+  return "Happy holidays!";
+}
+
+function happyHolidaysTo(name) {
+  return `Happy holidays, ${name}!`;
+}
+happyHolidaysTo("Romana");
+
+function happyHolidayTo(holiday,name) {
+  return `Happy ${holiday}, ${name}!`;
+}
+happyHolidayTo("Friday","Romana");
+
+function holidayCountdown(holiday,days) {
+  return `It's ${days} days until ${holiday}!`;
+}
+holidayCountdown("Two","Romana");
