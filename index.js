50844c47d1d0b7c8a614d4e1422542b8187deaf5
Jordan Farkas <jordfark@gmail.com>
2018-05-16 17:49:33 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// write your code below!
+function happyHolidays(string){return"Happy holidays!"}
+var nam = "Jordan";
+function happyHolidaysTo(name)
+{return`Happy holidays, ${name}!`}
+var nam = "Jordan";
+var holiday = "Hannukah";
+function happyHolidayTo(holiday,name)
+{return`Happy ${holiday}, ${name}!`}
+var holiday = "Mother's Day";
+var days = "20";
+function holidayCountdown(holiday, days)
+{return`It's ${days} days until ${holiday}!`}
