e6f7de746e51420ab59729fabb730ce835ad9f67
Eric Kouassi <hello@erickouassi.com>
2018-05-18 00:06:26 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // write your code below!
+function happyHolidays(){
+return 'Happy holidays!'
+}
+
+function happyHolidaysTo(name) {
+return 'Happy holidays, ${name}!'
+}
+
+happyHolidaysTo('Eric') // "Happy holidays, Eric"
+happyHolidaysTo('Solange') // "Happy holidays, Solange"
+happyHolidaysTo('Rhonda') // "Happy holidays, Rhonda"
+
+function happyHolidatTo(holiday,name){
+return 'Happy ${holiday}, ${name}!'
+}
