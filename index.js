8b8326004fbd3fc701672e89d46e7b83579fd371
Nate Eckerson <nate.eckerson@gmail.com>
2018-05-24 22:58:40 -0400

    add backticks

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
 // write your code below!
 
-fuction happyHolidays() {
+function happyHolidays() {
   return "Happy holidays!"
 }
 
 function happyHolidaysTo(name) {
-  return "Happy holidays ${name}!"
+  return `Happy holidays, ${name}!`
 }
 
 
 function happyHolidayTo(holiday, name) {
-  return "Happy ${holiday}, ${name}!"
+  return `Happy ${holiday}, ${name}!`
 }
 
 function holidayCountdown(holiday, days) {
-  return "It\'s ${days} days until ${holiday}!"
+  return `It\'s ${days} days until ${holiday}!`
 }
