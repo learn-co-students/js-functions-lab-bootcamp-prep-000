6299b28048615f028f68b6af7b90d27b7459046d
mary.e.beliveau@gmail.com Beliveau <mary.e.beliveau@gmail.com>
2018-05-29 18:41:20 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // write your code below!
+function happyHolidays() {
+  return "Happy holidays!"
+}
+
+happyHolidays()
+
+function happyHolidaysTo(name) {
+  console.log(`Happy holidays, ${name}!`)
+}
+
+happyHolidaysTo('Mary')
+
+function happyHolidayTo(holiday, name) {
+  console.log(`Happy ${holiday}, ${name}!`)
+}
+
+happyHolidaysTo('Christmas', 'Mary')
+
+function holidayCountdown(holiday, days) {
+  return `It's ${days} days until ${holiday}!`
+}
+
+holidayCountdown('Christmas', 5)
+
+
