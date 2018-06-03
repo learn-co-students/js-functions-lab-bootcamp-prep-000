9903bd7803703ab9ae76eaf7f472a5312f33c7f5
Derrick Skipwith <dskipwith15@gmail.com>
2018-06-03 18:10:24 +0000

    Done.

--- index.js
+++ index.js
@@ -10,6 +10,6 @@ function happyHolidayTo(holiday, name) {
   return "Happy Independence Day, you!"
 }
 
-function hoildayCountdown(holiday, days) {
+function holidayCountdown(holiday, days) {
   return `It's ${days} days until ${holiday}!`
 }
