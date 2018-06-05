44147669b2b74183d02f8da872267d2ee5a22cf3
Daniel O'Brien <dannyk85.dob@gmail.com>
2018-06-05 13:23:21 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // write your code below!
+
+// function to return happy Holidays no params
+function happyHolidays()
+{
+  return "Happy holidays!"
+}
+
+// happy holidays function 1 param for name
+function happyHolidaysTo(name)
+{
+  return `Happy holidays, ${name}!`
+}
+
+// happy holiday function 2 params for holiday, name
+function happyHolidayTo(holiday, name)
+{
+  return `Happy ${holiday}, ${name}!`
+}
+
+// holiday countdown function accepts 2 params
+// read param order carefully :)
+function holidayCountdown(holiday, days)
+{
+  return `It's ${days} days until ${holiday}!`
+}
