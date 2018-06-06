8753c4e6d645fb45800e0a802500663fe7ae7b86
gmat191@gmail.com <gmat191@gmail.com>
2018-06-06 23:58:55 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// write your code below!
+function happyHolidays(string) {
+  return("Happy holidays!")
+}
+function happyHolidaysTo(name) {
+  return(`Happy holidays, ${name}!`)
+}
+function happyHolidayTo(holiday, name){
+  return(`Happy ${holiday}, ${name}!`)
+  if(holiday){
+    (happyHolidayTo("Happy Independance Day to You!") === happyHolidayTo("Happy Independance Day to you!"))
+  }
+}
+function holidayCountdown( holiday, days) {
+  return(`It's ${days} days until ${holiday}!`)
+}
