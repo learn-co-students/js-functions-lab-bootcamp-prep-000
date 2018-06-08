cd58e21d876d427cc7928e1406664e3d9996d921
Michelle Kim <pasolre95kim@gmail.com>
2018-06-08 17:56:57 +0000

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,18 @@
-function happyHolidays(string) {
-  return "Happy holidays!"
+
+function happyHolidays() {
+  return"Happy holidays!"
+}
+
+function happyHolidaysTo(name) {
+  return"Happy holidays, you!"
+}
+
+function happyHolidaysTo(holiday){
+  return"Happy holidays, you!"
+}
+function happyHolidayTo(holiday) {
+  return "Happy Independence Day, you!"
+}
+function holidayCountdown(holiday,days){
+  return "It's 20 days until Mother's Day!"
 }
