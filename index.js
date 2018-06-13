c4bf438fa1d1154b4c8d950d7bf79035ff7ed493
PJ Valentini <pj.valentini@gmail.com>
2018-06-13 19:21:40 +0000

    Done.

--- index.js
+++ index.js
@@ -1,19 +1,22 @@
-// write your code below!
+// function that returns a string
 function happyHolidays() {
   return "Happy holidays!";
 }
 happyHolidays();
 
+// function takes 1 param and returns a template litteral
 function happyHolidaysTo(name) {
   return `Happy holidays, ${name}!`
 }
 happyHolidaysTo("PJ");
 
+// function takes 2 params and returns a template litteral
 function happyHolidayTo(holiday, name) {
   return `Happy ${holiday}, ${name}!`
 }
 happyHolidayTo("Kwanza", "PJ");
 
+// function takes 2 params and returns a template litteral
 function holidayCountdown(holiday, days) {
   return `It's ${days} days until ${holiday}!`
 } 
