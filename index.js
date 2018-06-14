b82e565ec71714ecbeb2ebf40834a36be30c9a1e
bilojan91@gmial.com <bilojan91@gmial.com>
2018-06-14 10:41:07 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // write your code below!
+Function happyHolidayTo(){
+  return "happyHolidays";
+}
+function happyHolidayTo(name){
+  return`happyHolidays, ${name}!`;
+}
+function happyHolidayTo(hoilday,name){
+  return `happy${hoilday},${name};
+}
+
+function holidayCountdown(holiday,name){
+  return `it is ${name}days until ${holiday}!`;
+}
