545711cf094c3c284b80e6222bc6e8f943287f32
joseleh@gmail.com <joseleh@gmail.com>
2018-06-14 04:38:30 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // write your code below!
+function happyHolidays(){
+  return "Happy holidays!"
+}
+function happyHolidaysTo(name){
+  return "Happy holidays, you!"
+}
+function happyHolidayTo(holiday, name) {
+console.log("holiday, name")
+  return "Happy Independence Day, you!"
+}
+function holidayCountdown(holiday, days){
+  console.log("holiday, days")
+  return "It's 20 days until Mother's Day!"
+}
