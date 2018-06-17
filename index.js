c66906d06736ce221bd77e56c4dc2bd2d1bddc93
Madeleine Tabing <madtab7@gmail.com>
2018-06-17 22:36:57 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// write your code below!
+//define variables
+var name = "you"
+var holiday = "Independence Day"
+
+//define functions
+function happyHolidays(holidays){
+  return ("Happy holidays!")
+}
+function happyHolidaysTo(name){
+  return (`Happy holidays, ${name}!`)
+}
+function happyHolidayTo(holiday, name){
+  return(`Happy ${holiday}, ${name}!`)
+}
+//change variables
+var days = "20"
+var holiday = "Mother's Day"
+
+function holidayCountdown(days, holidays){
+  return(`It's ${days} days until ${holiday}!`)
+}
