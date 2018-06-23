60cf6490d7b78d5c8064069eb744a1306bf82b0d
Samuel Watson <otacon100@hotmail.com>
2018-06-23 18:45:03 +0000

    Done.

--- index.js
+++ index.js
@@ -1,6 +1,3 @@
-holiday = 'Birthday'
-name = 'Katie'
-days = 20
 
 
 function happyHolidays() {
@@ -8,16 +5,13 @@ function happyHolidays() {
 }
 
 function happyHolidaysTo(name) {
-  console.log (`Happy holidays, ${name}!`)
   return (`Happy holidays, ${name}!`)
 }
 
 function happyHolidayTo (holiday, name) {
-  console.log (`Happy ${holiday}, ${name}!`)
   return (`Happy ${holiday}, ${name}!`)
 }
 
 function holidayCountdown (holiday, days) {
-  console.log(`It's ${days} days until ${holiday}!`)
   return (`It's ${days} days until ${holiday}!`)
 }
