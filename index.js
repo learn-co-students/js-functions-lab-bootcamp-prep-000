f4f64af450e499785315780e131650f4bf551906
schmuhl2@outlook.com <schmuhl2@outlook.com>
2018-06-30 23:28:42 +0000

    Done.

--- index.js
+++ index.js
@@ -2,3 +2,19 @@
 function happyHolidays(string) {
   return "Happy holidays!"
 }
+function happyHolidays() {
+  return "Happy holidays!"
+}
+
+function happyHolidaysTo(name) {
+  return `Happy holidays, ${name}!`
+}
+
+function happyHolidayTo(holiday, name) {
+  return `Happy ${holiday}, ${name}!`
+}
+
+function holidayCountdown(holiday, days) {
+  return `It's ${days} days until ${holiday}!`
+}
+
