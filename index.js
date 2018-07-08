8389ad3a30ba523a47d3b2b3e43970b6f3d8af3e
Amaris <monyei96@hotmail.com>
2018-07-08 18:12:18 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // write your code below!
+function happyHolidays(){
+  return "Happy holidays!";
+}
+function happyHolidaysTo($name){
+  $name = "you!";
+  return "Happy holidays," + " " + "you!";
+  }
+function happyHolidayTo($holiday, $name){
+  $holiday = "Independence Day";
+  $name = "you";
+  return "Happy" + " " + "Independence Day," + " " + "you" + "!";
+  }
+  
+function holidayCountdown($holiday, $days){
+  $holiday = "Mother's Day";
+  $days = "20";
+  return "It's" + " " + "20" + " " + "days until" + " " + "Mother's Day" + "!";
+  
+}
