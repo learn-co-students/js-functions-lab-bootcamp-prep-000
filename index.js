fef599f6f98e4b5c728263f5cd62fe06e4471e29
Ashley Pereira <ashleypereira70@gmail.com>
2018-07-15 16:36:32 +0000

    Done.

--- index.js
+++ index.js
@@ -1,7 +1,3 @@
-
-describe('functions', () => {
-  describe('happyHolidays', () => {
-    it('returns "Happy holidays!"', () => {
-      expect(happyHolidays()).toEqual('Happy holidays!')
-    })
-  })
+	function happyHolidays() {
+	console.log("Happy,Holidays!")
+	}
