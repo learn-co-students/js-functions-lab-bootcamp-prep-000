96c1af6888ce99309e69c8f20ad0f596af52fede
Jeb Henrie <jebhenrie@gmail.com>
2018-07-26 18:05:11 +0000

    Done.

--- index.js
+++ index.js
@@ -7,3 +7,6 @@ function happyHolidaysTo(name){
 function happyHolidayTo(holiday,name){
   return "Happy Independence Day, you!"
 }
+function holidayCountdown(holiday,days){
+  return "It's 20 days until Mother's Day!"
+}
