ef49d01b230c1fcf3e7350329c6f06f314341292
ypeeyore2@yahoo.com <ypeeyore2@yahoo.com>
2018-07-28 03:51:33 +0000

    Done.

--- index.js
+++ index.js
@@ -17,15 +17,16 @@ function holidayCountdown(holiday, days) {
   return "It's ${days} days until${holiday}!"
 }
 
-/*
+
 var testname = "Santa"
 console.log($testname) // NOT DISPLAYED
 console.log("this is my,$testname") // NOT DISPLAYED
- */
+
 console.log("Starting Function Lab.") // NOT DISPLAYED
 
 happyHolidays
 happyHolidaysTo("you")
+happyHolidayTo("Blue Moon Day", "Blues Brothers")
 holidayCountdown("Labor Day", "40")
 
 console.log("Finished Function Lab.") // NOT DISPLAYED
