4631dc0281ef24ee640c6bf48d78ea7c3412ef01
ypeeyore2@yahoo.com <ypeeyore2@yahoo.com>
2018-07-28 15:03:08 +0000

    Done.

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ function happyHolidays() {
   return "Happy holidays!"
 }
 
-function happyHolidaysTo(name)
+function happyHolidaysTo(name) {
 // function happyHolidaysTo(name="you!") {
 // hard-coded passed
 //  return "Happy holidays, you!"
