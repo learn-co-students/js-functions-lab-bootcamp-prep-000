45a41da05671105154d2266b56619cf298b4d056
david m flynn <flynndave1@msn.com>
2018-07-28 04:18:17 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // write your code below!
+//Happy Coding Day
+
+var md  = "Mother\'s Day";
+
+function happyHolidays ()  {
+  return ("Happy holidays!");
+}
+
+function happyHolidaysTo (namen) {
+  return( 'Happy holidays, ' + namen + '!' ) ;
+}
+
+function happyHolidayTo (theDay , namen) {
+  return  ("Happy " + theDay + "\, "  + namen + "!" ) 
+}
+
+function holidayCountdown (theDay ,  daysTill) {
+  return  ("It\'s " +  daysTill + " days until " + theDay +"!" ) 
+}
+
+
+
+  
+  console.log( happyHolidays()) ;
+  console.log( happyHolidaysTo( "James"));
+  console.log( happyHolidayTo(md ,"James"));
+  console.log( holidayCountdown(md , 25));
+  
+  
