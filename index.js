5b77ceb5baa44784d834fc64d0a48836647edb13
Nicolas Marcora <nicolas.marcora@yahoo.co.uk>
2018-07-28 19:18:42 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // write your code below!
+const happyHolidays = () =>
+  'Happy holidays!'
+  
+const happyHolidaysTo = name =>
+  `Happy holidays, ${name}!`
+
+const happyHolidayTo = (holiday, name) =>
+  `Happy ${holiday}, ${name}!`
+  
+const holidayCountdown = (holiday, days) =>
+  `It's ${days} days until ${holiday}!`
+
+module.exports = {
+  happyHolidays,
+  happyHolidaysTo,
+  happyHolidayTo,
+  holidayCountdown
+}
