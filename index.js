e2bbf30ef1514571b23133846854765ff3918d02
ypeeyore2@yahoo.com <ypeeyore2@yahoo.com>
2018-07-28 14:47:39 +0000

    Done.

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ function happyHolidays() {
 function happyHolidaysTo(name='you!') {
 //  return "Happy holidays, you!" // hard-coded passed
 //  return 'Happy holidays, ${name}!' // Error when no argument and when argument = "you" or 'you'
-  return "Happy holidays, ${name}!"
+  return 'Happy holidays, ${name}!'
 }
 /*
 function happyHolidayTo(holiday="Independence Day", name="you!") {
