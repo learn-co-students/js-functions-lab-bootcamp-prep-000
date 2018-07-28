20f8edbd4c66547f1b5de49311933828e749308c
Rina Schwartz <rinadschwartz@protonmail.ch>
2018-07-28 19:31:32 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // write your code below!
+//function with no parameters
+function happyHolidays () {
+  return "Happy holidays!"
+}
+  
+//function with one parameter
+function happyHolidaysTo (name){
+  return `Happy holidays, ${name}!`
+}
+//function with two paramteters
+function happyHolidayTo (holiday, name){
+  return `Happy ${holiday}, ${name}!`
+}
+//another function with two paramters
+function holidayCountdown (holiday, days){
+  return `It's ${days} days until ${holiday}!`
+}
