fe4620d7c82ee0dfadfe5b104e7a84cf7fccc374
ypeeyore2@yahoo.com <ypeeyore2@yahoo.com>
2018-07-28 02:54:40 +0000

    Done.

--- index.js
+++ index.js
@@ -7,26 +7,25 @@ function happyHolidays() {
 function happyHolidaysTo(name) {
   return "Happy holidays, ${name}!"
 }
-/*
-functions happyHolidayTo(string, string) {
-  var holiday = string
-  var name = string
-  returns "Happy ${holiday}, ${name}!"
+
+functions happyHolidayTo(holiday, name) {
+  return "Happy ${holiday}, ${name}!"
 }
 
 function holidayCountdown(holiday, days) { 
-  returns "It's ${days} days until${holiday}!"
+  return "It's ${days} days until${holiday}!"
 }
- */
+
 /*
 var testname = "Santa"
 console.log($testname)
 console.log("this is my,$testname")
- 
+ */
 console.log("Starting Function Lab.") // NOT DISPLAYED
 happyHolidays
 console.log("Finished Function Lab.") // NOT DISPLAYED
 console.log(" ") // not DISPLAYED
 
 happyHolidaysTo("you")
- */
+
+holidayCountdown("Labor Day", "40")
