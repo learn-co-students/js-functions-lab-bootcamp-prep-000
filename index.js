c067975b1f367b6ac623673338afa5192465975a
ypeeyore2@yahoo.com <ypeeyore2@yahoo.com>
2018-07-28 14:38:51 +0000

    Done.

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ function happyHolidays() {
 
 function happyHolidaysTo(name="you!") {
 //  return "Happy holidays, you!" // hard-coded passed
-  return "Happy holidays, ${name}"
+  return "Happy holidays, ${name}" // 
 }
 /*
 function happyHolidayTo(holiday="Independence Day", name="you!") {
@@ -27,7 +27,7 @@ console.log("this is my,$testname") // NOT DISPLAYED
 console.log("Starting Function Lab.") // NOT DISPLAYED
 
 happyHolidays
-happyHolidaysTo()
+happyHolidaysTo("you")
 // happyHolidaysTo("you")
 /*
 happyHolidayTo("Independence Day", "you")
