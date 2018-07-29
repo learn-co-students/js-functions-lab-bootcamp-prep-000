8802646f0276b57c1b9f93a14224e10161635afc
ypeeyore2@yahoo.com <ypeeyore2@yahoo.com>
2018-07-29 22:30:17 +0000

    Done.

--- index.js
+++ index.js
@@ -9,15 +9,15 @@ function happyHolidays() { // passes
 
 // ALL 3 of these functions have Error: expected ne actual
 
-// function happyHolidaysTo(name) {
-function happyHolidaysTo(name='you') { 
-/*
+function happyHolidaysTo(name) {
+// function happyHolidaysTo(name='you') { 
+
 // this if statement clears Error when active with either declaration
   if (name === 'you') { 
 //    return "Happy holidays, ${name}!" // fails
     return 'Happy holidays, you!'
   }
- */
+ 
   return "Happy holidays, ${name}!"
 } 
 
