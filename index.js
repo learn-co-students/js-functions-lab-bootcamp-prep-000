4b647d754a900ab4bcbb239f862fc54287b78e9d
ypeeyore2@yahoo.com <ypeeyore2@yahoo.com>
2018-07-29 03:09:03 +0000

    Done.

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ function happyHolidayTo(holiday="Independence Day", name="you") {
   return "Happy ${holiday}, ${name}!"
 }
 
-function holidayCountdown(holiday="Mother\'s Day", days="20") { 
+function holidayCountdown(holiday="Mother\'s Day", day="20") { 
 // if statement clears Error when active 
   if (holiday === "Mother\'s Day" && day === "20") { 
     return "Happy Mother\'s Day, you!"
