f8557908d7297775796ba0ee7d05c509a1fe629b
ypeeyore2@yahoo.com <ypeeyore2@yahoo.com>
2018-07-29 13:27:34 +0000

    Done.

--- index.js
+++ index.js
@@ -60,8 +60,10 @@ happyHolidays // passes
 // happyHolidaysTo() // passes when workaround is active
 happyHolidaysTo('you') // passes when workaround is active
 
+// happyHolidayTo()
 happyHolidayTo('Independence Day', 'you') // passes when workaround is active
 
+// holidayCountdown()
 holidayCountdown('Mother\'s Day', 20) // passes when workaround is active
 
 console.log("Finished Function Lab.") // DOES NOT DISPLAY
