003f68e0c038beaeb9853d77999a1656df7f1a3b
ypeeyore2@yahoo.com <ypeeyore2@yahoo.com>
2018-07-29 22:04:45 +0000

    Done.

--- index.js
+++ index.js
@@ -8,22 +8,22 @@ My global variables
 /*
 My functions
  */
-/*
+
 function happyHolidays() { // passes
   return 'Happy holidays!'
 }
- */
+
 // ALL 3 of these functions have Error: expected ne actual
 /*
 function happyHolidaysTo() {
 // function happyHolidaysTo(name='you') { 
 // this if statement clears Error when active with either declaration
-// /*
+
   if (name === 'you') { 
 //    return "Happy holidays, ${name}!" // fails
     return 'Happy holidays, you!'
   }
-// */
+
   return "Happy holidays, ${name}!"
 } 
  */
@@ -31,12 +31,12 @@ function happyHolidaysTo() {
 function happyHolidayTo(holiday, name) {
 // function happyHolidayTo(holiday='Independence Day', name='you') { 
 // this if statement clears Error when active with either declaration
-// /*
+
   if (holiday === 'Independence Day' && name === 'you') { 
 //    return "Happy ${holiday}, ${name}!" // fails
     return 'Happy Independence Day, you!'
   }
-// */
+
   return "Happy ${holiday}, ${name}!" 
 }
  */
@@ -44,12 +44,12 @@ function happyHolidayTo(holiday, name) {
 function holidayCountdown(holiday, days) {
 // function holidayCountdown(holiday='Mother\'s Day', days=20) { 
 // this if statement clears Error when active with either declaration
-// /*
+
   if (holiday === 'Mother\'s Day' && days === 20) { 
 //    return "It\'s ${days} days until ${holiday}!" // fails
     return "It\'s 20 days until Mother\'s Day!"
   }
-// */
+
   return "It\'s ${days} days until ${holiday}!"
 }
 */ 
