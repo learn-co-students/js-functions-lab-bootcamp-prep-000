8a22bd035c91be449e4637c1a9afa93cf067bbcf
ypeeyore2@yahoo.com <ypeeyore2@yahoo.com>
2018-07-30 17:23:42 +0000

    Done.

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ function happyHolidaysTo(name) {
 function happyHolidaysTo(name='you') { 
  */
   if (name === 'you') { // used with if to confirm parameter assignment since console.log is not working
-    return 'Happy holidays, you!'
+    return 'my Happy holidays, you!'
   }
 
   return "Happy holidays, ${name}!"
@@ -25,7 +25,7 @@ function happyHolidayTo(holiday, name) {
 // function happyHolidayTo(holiday='Independence Day', name='you') { 
  */
   if (holiday === 'Independence Day' && name === 'you') { // used with if to confirm parameter assignment since console.log is not working
-    return 'Happy Independence Day, you!'
+    return 'my Happy Independence Day, you!'
   }
 
   return "Happy ${holiday}, ${name}!" 
@@ -36,7 +36,7 @@ function holidayCountdown(holiday, days) {
 // function holidayCountdown(holiday='Mother\'s Day', days=20) { 
  */
   if (holiday === 'Mother\'s Day' && days === 20) { // used with if to confirm parameter assignment since console.log is not working
-    return "It\'s 20 days until Mother\'s Day!"
+    return "my It\'s 20 days until Mother\'s Day!"
   }
 
   return "It\'s ${days} days until ${holiday}!"
