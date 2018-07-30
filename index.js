ade3d7f3160c73f120b82421fcc4444be0a77103
ypeeyore2@yahoo.com <ypeeyore2@yahoo.com>
2018-07-30 09:49:13 +0000

    Done.

--- index.js
+++ index.js
@@ -9,47 +9,37 @@ function happyHolidays() { // passes
 
 // ALL 3 of these functions have Error: expected ne actual
 
-function happyHolidaysTo(string) {
-// function happyHolidaysTo(name='you') { 
-  var name = string
+function happyHolidaysTo(name) {
 /*
-// this if statement clears Error when active with either declaration
+function happyHolidaysTo(name='you') { // used with if to confirm parameter assignment since console.log is not working
   if (name === 'you') { 
-//    return "Happy holidays, ${name}!" // fails
     return 'Happy holidays, you!'
   }
  */ 
-
   return "Happy holidays, ${name}!"
-
 } 
 
-/*
 function happyHolidayTo(holiday, name) {
-// function happyHolidayTo(holiday='Independence Day', name='you') { 
-// this if statement clears Error when active with either declaration
-
+/*
+// function happyHolidayTo(holiday='Independence Day', name='you') { // used with if to confirm parameter assignment since console.log is not working
   if (holiday === 'Independence Day' && name === 'you') { 
 //    return "Happy ${holiday}, ${name}!" // fails
     return 'Happy Independence Day, you!'
   }
-
+ */
   return "Happy ${holiday}, ${name}!" 
 }
- */
-/*
-function holidayCountdown(holiday, days) {
-// function holidayCountdown(holiday='Mother\'s Day', days=20) { 
-// this if statement clears Error when active with either declaration
 
+function holidayCountdown(holiday, days) {
+/*
+// function holidayCountdown(holiday='Mother\'s Day', days=20) { // used with if to confirm parameter assignment since console.log is not working
   if (holiday === 'Mother\'s Day' && days === 20) { 
-//    return "It\'s ${days} days until ${holiday}!" // fails
     return "It\'s 20 days until Mother\'s Day!"
   }
-
+ */
   return "It\'s ${days} days until ${holiday}!"
 }
-*/ 
+
 /*
 End of my functions
  */
@@ -63,15 +53,15 @@ console.log("Starting Function Lab.") // DOES NOT DISPLAY
 happyHolidays // passes
 happyHolidays() // Also passes
  
-happyHolidaysTo // passes when workaround is active
-happyHolidaysTo('you') // passes when workaround is active
-happyHolidaysTo('me') // fails
+happyHolidaysTo 
+happyHolidaysTo('you') 
+happyHolidaysTo('me') 
 
-happyHolidayTo() // passes when workaround is active
-happyHolidayTo('Independence Day', 'you') // passes when workaround is active
+happyHolidayTo() 
+happyHolidayTo('Independence Day', 'you') 
 
-holidayCountdown() // passes when workaround is active
-holidayCountdown('Mother\'s Day', 20) // passes when workaround is active
+holidayCountdown() 
+holidayCountdown('Mother\'s Day', 20) 
 
 console.log("Finished Function Lab.") // DOES NOT DISPLAY
 
