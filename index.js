b3d85b3d98bd9a1d3d65edf89da7396c4d1803ca
ypeeyore2@yahoo.com <ypeeyore2@yahoo.com>
2018-07-31 18:24:27 +0000

    Done.

--- index.js
+++ index.js
@@ -7,18 +7,11 @@ function happyHolidays() {
 function happyHolidaysTo(name) {
   return "Happy holidays, ${name}!"
 } 
-/*
+
 function happyHolidayTo(holiday, name) {
-// function happyHolidayTo(holiday='Independence Day', name='you') { 
-  if (holiday === 'Independence Day' && name === 'you') { // used to confirm arguments OK since console.log is not working
-    // return 'my Happy Independence Day, you!' // 1st test result OK with either declaration
-    return "Happy ${holiday}, ${name}!" // 2nd test result FAILS with either declaration
-  }
-  
-  console.log("happyHolidayTo holiday is  ${holiday} and name is ${name}")
   return "Happy ${holiday}, ${name}!" 
 }
-
+/*
 function holidayCountdown(holiday, days) {
 // function holidayCountdown(holiday='Mother\'s Day', days=20) { 
   if (holiday === 'Mother\'s Day' && days === 20) { // used to confirm parameters are OK since console.log is not working
@@ -30,19 +23,10 @@ function holidayCountdown(holiday, days) {
   return "It\'s ${days} days until ${holiday}
 }
  */
-console.log('Starting Function Lab.') // DOES NOT DISPLAY
 
 happyHolidays() 
- 
-
 happyHolidaysTo('you') 
-/*
-// happyHolidayTo() 
 happyHolidayTo('Independence Day', 'you') 
-
-// holidayCountdown() 
+/*
 holidayCountdown('Mother\'s Day', 20) 
-
-console.log('Finished Function Lab.') // DOES NOT DISPLAY
-
  */
