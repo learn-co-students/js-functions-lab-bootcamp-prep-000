ffb82f9b21c8bc2739a2bc66115edbaa36251c4b
ypeeyore2@yahoo.com <ypeeyore2@yahoo.com>
2018-07-31 18:27:55 +0000

    Done.

--- index.js
+++ index.js
@@ -11,18 +11,10 @@ function happyHolidaysTo(name) {
 function happyHolidayTo(holiday, name) {
   return "Happy ${holiday}, ${name}!" 
 }
-/*
-function holidayCountdown(holiday, days) {
-// function holidayCountdown(holiday='Mother\'s Day', days=20) { 
-  if (holiday === 'Mother\'s Day' && days === 20) { // used to confirm parameters are OK since console.log is not working
-    // return "my It\'s 20 days until Mother\'s Day!" // 1st test result OK with either declaration
-    return "It\'s ${days} days until ${holiday}!" // 2nd test result FAILS with either declaration
-  }
 
-  console.log("holidayCountdown holiday is ${holiday} and days is ${days}")
-  return "It\'s ${days} days until ${holiday}
+function holidayCountdown(holiday, days) {
+  return "It\'s ${days} days until ${holiday}"
 }
- */
 
 happyHolidays() 
 happyHolidaysTo('you') 
