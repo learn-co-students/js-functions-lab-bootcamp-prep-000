3fa3c16d0cb67c355e29ec11b191495b071b4cea
ypeeyore2@yahoo.com <ypeeyore2@yahoo.com>
2018-07-31 19:32:13 +0000

    Done.

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ function happyHolidayTo(holiday, name) {
 }
 
 function holidayCountdown(holiday, days) {
-  return `It\'s ${days} days until ${holiday}`
+  return `It\'s ${days} days until ${holiday}!`
 }
 
 
