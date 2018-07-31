9568216b1fb9d7953eef336eb40016f40d5d119a
ypeeyore2@yahoo.com <ypeeyore2@yahoo.com>
2018-07-31 18:52:35 +0000

    Done.

--- index.js
+++ index.js
@@ -5,21 +5,15 @@ function happyHolidays() {
 }
 
 function happyHolidaysTo(name) {
-  // return "Happy holidays, ${name}!"
-  return "Happy holidays, you!"
+  return "Happy holidays, ${name}!"
 } 
 
 function happyHolidayTo(holiday, name) {
-  //actual
   return "Happy ${holiday}, ${name}!"
-  //expected return "Happy Independence Day, you!"
 }
 
 function holidayCountdown(holiday, days) {
-  //actual
-  // return "It\'s ${days} days until ${holiday}"
-  //expected 
-  return "It\'s 20 days until Mother\'s Day!"
+  return "It\'s ${days} days until ${holiday}"
 }
 
 {
