d0e40cb0d9a47a4657bf0e57ff4af035d08a218d
Roundabout Rider <ntm.email@icloud.com>
2018-08-04 14:50:36 +0000

    Done.

--- index.js
+++ index.js
@@ -1,17 +1,16 @@
 // write your code below!
-function happyHolidays(string){
-  if (string === '')
-  return 'Happy Holidays!';
+function happyHolidays(){
+  return "Happy Holidays!"
 }
 
-function happyHolidaysTo(string){
-  return 'Happy holidays, ${name}!';
+function happyHolidaysTo(name){
+  return 'Happy holidays, ${name}!'
 }
 
-function happyHolidaysTo(holiday , name){
-  return 'Happy ${holiday}, ${name}!';
+function happyHolidaysTo(holiday, name){
+  return 'Happy ${holiday}, ${name}!'
 }
 
-function holidayCountdown(days , holiday){
+function holidayCountdown(days, holiday){
   return 'It\'s ${days} days until ${holiday}!'
 }
