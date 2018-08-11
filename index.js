f8229836bfeccde6614589543f34d696b91143ad
Amy Frenzel <frenzel.amy@gmail.com>
2018-08-11 19:33:06 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// write your code below!
+var name = "Echo"
+var holiday = "New Years"
+var days = "42"
+
+function happyHolidays() {
+  return `Happy holidays!`
+} 
+
+function happyHolidaysTo() {
+  return `Happy holidays, ${name}!`
+}
+
+function happyHolidayTo() {
+  return `Happy ${holiday}, ${name}!`
+  
+}
+
+function holidayCountdown() {
+  return `It's ${days} days until ${holiday}!`
+}
