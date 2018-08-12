92f10a96112612b4742c8b3bde9eeff03aff2d4c
Oomkar Pandit <oomkar@yahoo.com>
2018-08-12 09:27:37 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // write your code below!
+function happyHolidays(){
+  return "Happy holidays!"
+}
+// Second test - with parameter
+function happyHolidaysTo(name){
+  return `Happy holidays, ${name}!`
+}
+//Third Test - with 2 parameters
+function happyHolidayTo(holiday,name){
+  return `Happy ${holiday}, ${name}!`
+}
+//Fourth test- with 2 parameters count down
+function holidayCountdown(holiday,days){
+  return `It's ${days} days until ${holiday}!`
+}
