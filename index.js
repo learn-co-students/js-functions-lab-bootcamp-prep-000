cd818d977fe6c48c3b8994acb155b48fbec67a5e
gc6123@bard.edu <gc6123@bard.edu>
2018-08-16 06:25:24 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // write your code below!
+function happyHolidays(string) {return "Happy holidays!"}
+function happyHolidaysTo(Max) {return `Happy holidays, ${Max}!`}
+function happyHolidayTo(Christmas, Charles) {
+  return `Happy ${Christmas}, ${Charles}!`}
+function holidayCountdown(birthday, seven) {
+  return `It's ${seven} days until ${birthday}!`}
