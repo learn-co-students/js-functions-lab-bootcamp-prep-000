20249bc1fd3a6d9148abc89d56f4343ea95f43bd
tatiana.mojica@bustle.com <tatiana.mojica@bustle.com>
2018-08-18 17:40:57 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
 // write your code below!
+/*global describe, it*/
+
+describe('functions', () => {
+  describe('happyHolidays', () => {
+    it('returns "Happy holidays!"', () => {
+      expect(happyHolidays()).toEqual('Happy holidays!')
+    })
+  })
+
+  describe('happyHolidaysTo(name)', () => {
+    it('returns "Happy holidays, ${name}!"', () => {
+      expect(happyHolidaysTo('you')).toEqual('Happy holidays, you!')
+    })
+  })
+
+  describe('happyHolidayTo(holiday, name)', () => {
+    it('returns "Happy ${holiday}, ${name}!"', () => {
+      expect(happyHolidayTo('Independence Day', 'you')).toEqual('Happy Independence Day, you!')
+    })
+  })
+
+  describe('holidayCountdown(holiday, days)', () => {
+    it('returns "It\'s ${days} days until ${holiday}!"', () => {
+      expect(holidayCountdown("Mother's Day", 20)).toEqual("It's 20 days until Mother's Day!")
+    })
+  })
+})
+
+function happyHolidays () {
+  return "Happy holidays!"
+}
+  function happyHolidaysTo(name) {
+    return `Happy holidays, ${name}!`
+  }
+  function happyHolidayTo(holiday, name) {
+    return `Happy ${holiday}, ${name}!`
+  }
+  function holidayCountdown(holiday, days) {
+    return `It\'s ${days} days until ${holiday}!`
+  }
+    
