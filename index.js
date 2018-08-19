ebfab70bcce3738e7de9a5b2552a437ec91fd7f0
Dwayne Munro <dwaynemunro1845@gmail.com>
2018-08-19 21:42:10 +0000

    Done.

--- index.js
+++ index.js
@@ -5,13 +5,13 @@ function happyHolidays() {
 }
 
 function happyHolidaysTo(name) {
-  return `Happy holidays, ${name}`;
+  return `Happy holidays, ${name}!`;
 }
 
 function HappyHolidayTo(holiday, name) {
-  return `Happy ${holiday}, ${name}`;
+  return `Happy ${holiday}, ${name}!`;
 }
 
 function holidayCountdown(days, holiday) {
-  return `It's ${days} days until ${holiday}`;
+  return `It's ${days} days until ${holiday}!`;
 }
