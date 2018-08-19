12b338cae4d3150f4484d739689108ef407e2855
Glorious Lemon <gloriouslemon@gmail.com>
2018-08-19 14:01:02 +0000

    Done.

--- index.js
+++ index.js
@@ -1,30 +1,28 @@
-// write your code below!
-
-var name = "emilie"
-var holiday = "summer"
-var number = "20"
+var name = "emilie";
+var holiday = "summer";
+var number = "20";
 
 function happyHolidays() {
-  return "Happy holidays!"
+  return "Happy holidays!";
 }
 
 function happyHolidaysTo(name) {
-  return `Happy holidays, ${name}!`
+  return `Happy holidays, ${name}!`;
 }
 
 function happyHolidayTo(holiday, name) {
-  return `Happy ${holiday}, ${name}!`  
+  return `Happy ${holiday}, ${name}!`;
 }
 
 function holidayCountdown(holiday, days) {
-  return `It's ${days} days until ${holiday}!`  
+  return `It's ${days} days until ${holiday}!`;  
 }
 
-console.log (happyHolidays())
+console.log (happyHolidays());
 
-console.log (happyHolidaysTo("Chris"))
+console.log (happyHolidaysTo("Chris"));
 
-console.log (happyHolidayTo("Birthday", "Chris"))
+console.log (happyHolidayTo("Birthday", "Chris"));
 
 console.log (holidayCountdown("Birthday", "3"))
 
