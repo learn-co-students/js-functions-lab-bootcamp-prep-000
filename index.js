23c528ce1bfc409d58e22e0d68bb8bbd43ecd505
Jamie Allbright <jamie.allbright@aol.com>
2018-08-20 20:35:48 +0000

    Done.

--- index.js
+++ index.js
@@ -13,6 +13,6 @@ function happyHolidayTo(holiday, name) {
 happyHolidayTo(`Independence Day`, `you`)
 
 function holidayCountdown(days, holiday) {
-  return (`It's ${days} until ${holiday}!`)
+  return (`It's ${days} days until ${holiday}!`)
 }
-holidayCountdown(20 , `Mother's Day`)
+holidayCountdown( 20 , `Mother's Day`)
