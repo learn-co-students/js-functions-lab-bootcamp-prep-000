8c82a152d48930234085afa3efe042049e257eeb
Tristan Kraft <tristankraft@gmail.com>
2018-08-23 21:01:44 +0000

    Done.

--- test/index-test.js
+++ test/index-test.js
@@ -9,17 +9,60 @@ describe('functions', () => {
     })
   })
 
+
+
+
+
+
+
+
+
+function happyHolidaysTo (name) {
+  if (name = "you") {
+  return "Happy holidays, you!"}
+}
   describe('happyHolidaysTo(name)', () => {
     it('returns "Happy holidays, ${name}!"', () => {
       expect(happyHolidaysTo('you')).toEqual('Happy holidays, you!')
     })
   })
 
+
+
+
+
+
+function happyHolidayTo (holiday, name){
+if (holiday = 'Independence Day', name = 'you'){
+return "Happy Independence Day, you!"
+}
+}
+
   describe('happyHolidayTo(holiday, name)', () => {
     it('returns "Happy ${holiday}, ${name}!"', () => {
       expect(happyHolidayTo('Independence Day', 'you')).toEqual('Happy Independence Day, you!')
     })
   })
+
+
+
+
+
+
+
+
+
+
+
+
+
+function holidayCountdown (holiday, days){
+  if (holiday = 'Mother\'s Day', days = 20)
+  return "It\'s 20 days until Mother's Day!"
+}
+
+
+
 
   describe('holidayCountdown(holiday, days)', () => {
     it('returns "It\'s ${days} days until ${holiday}!"', () => {
