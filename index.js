a5f65166d98ee16e264a40235fae13934b5a902e
Sabu <sabuw319@gmail.com>
2018-08-26 15:04:28 +0000

    Done.

--- index.js
+++ index.js
@@ -1,17 +1,18 @@
 // write your code below!
 
-function happyHolidays() {
-  return "Happy holidays!"
+function happyHolidays()
+   {return "Happy holidays!"
 }
 
-function happyHolidaysTo(name){
-  return "Happy holidays, ${name}!"
+function happyHolidaysTo(name)
+   {return "Happy holidays, ${name}!"
 }
 
-function happyHolidasTo(holiday, name){
-  return "Happy ${holiday}, ${name}"
+function happyHolidayTo(holiday, name)
+   {return `Happy ${holiday}, ${name}!`
 }
 
-function holidayCountdown(holiday, days){
-  return "It's ${days} days until ${holiday}!"
+function holidayCountdown(holiday, days)
+   {return `It\'s ${days} days until ${holiday}!`
 }
+
