5ed4d906c1dd39717e193aecabca9b7c8f850c18
brindab50@gmail.com behal <brindab50@gmail.com>
2018-08-29 18:45:52 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// write your code below!
+function happyHolidays() {
+  return("Happy holidays!")
+}
+happyHolidays()
+function happyHolidaysTo(name) {
+return(`Happy holidays,${name}!`)
+}
+happyHolidaysTo("Brinda")
+
+function happyHolidaysTo(name, holiday) {
+  return "happy ${Thanksgiving}, ${Brinda Behal}!"
+}
