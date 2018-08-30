af01e17d870ca1054159bdbb23108e8604528ffe
boodieroneil@gmail.com <boodieroneil@gmail.com>
2018-08-30 00:30:56 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // write your code below!
+function happyHolidays() {
+  return "Happy Holidays!"
+}
+
+function happyHolidaysTo(name) {
+  return `Happy holidays, ${name}!`
+  happyHolidaysTo("Roneil")
+}
+function happyHolidayTo(New Years, Ron) {
+  return `Happy ${holiday}, to ${name}!`
+}
+function holidayCountdown(days, holiday) {
+  console.log(15, "New Years")
+  return `It's ${days} days until ${holiday}!`
+}
+}
