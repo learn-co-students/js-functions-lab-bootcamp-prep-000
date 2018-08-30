322b132d8873ad5c9723323dc19220bd4e413393
Damia Bolling <dbolling75@gmail.com>
2018-08-30 02:12:39 +0000

    Done.

--- index.js
+++ index.js
@@ -1,4 +1,18 @@
 // write your code below!
 function sayhappyHolidays() {
-  console.log('Happy Holidays!')
+  return(`Happy Holidays!`);
 }
+
+
+function sayhappyHolidaysTo(Bob) {
+  return(`Happy Holidays, ${firstName}!`);
+}
+
+function say(greeting, firstName) {
+  console.log(`${greeting}, ${firstName}!`);
+}
+
+function sayholidayCountdown() { 
+  console.log(`It's ${days} days until ${holiday}!`);
+}
+
