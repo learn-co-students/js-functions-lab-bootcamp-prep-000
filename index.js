d8ce441acc1871735444593d2d5a51c67d6ff8c8
Lyn <jen.l@mailfence.com>
2018-09-06 21:13:24 +0000

    Done.

--- index.js
+++ index.js
@@ -7,11 +7,15 @@ function happyHolidaysTo (name) {
   return `Happy holidays, ${name}!`;
 }
 
-function happyHolidaysTo (holiday, name) { 
- return `Happy ${holiday}, ${name}!`;
+happyHolidaysTo();
+
+function happyHolidayTo (holiday, name) { 
+ return `Happy ${holiday}, ${name}!`
 }
 
+happyHolidayTo();
+
 function holidayCountdown (days, holiday) {
- return `It's ${days} days until ${holiday}!`;
+ return `It's ${holiday} days until ${days}!`;
 }
 
