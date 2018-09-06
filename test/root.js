d698a8b664250fc45d54e86d1647222eceed4ccd
ca.fernando415@gmail.com <ca.fernando415@gmail.com>
2018-09-06 03:32:01 +0000

    Done.

--- test/root.js
+++ test/root.js
@@ -1,9 +1,9 @@
-global.expect = require('expect')
+global.expect = require('expect');
 
-const fs = require('fs')
-const jsdom = require('mocha-jsdom')
-const path = require('path')
+const fs = require('fs');
+const jsdom = require('mocha-jsdom');
+const path = require('path');
 
 jsdom({
   src: fs.readFileSync(path.resolve(__dirname, '..', 'index.js'), 'utf-8')
-})
+});
