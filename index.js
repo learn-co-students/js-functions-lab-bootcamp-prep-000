4dd6222990d9042a8dca4048fc1ac2337ad1a714
Anthony Panayiotou <tonysteno@live.com>
2018-09-07 23:20:37 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // write your code below!
+Function ;"happyHolidays"()
+ console.log say "(happyHolidays)"
+
+Function ;"happyHolidaysTo"
+`happyHolidays, ${name} !`
+Function holidayCountdown `its ${days} until ${holiday}!`
