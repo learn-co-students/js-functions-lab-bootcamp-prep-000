ab656fa6c851206171b1492bf1c3f68640363d27
Todd R Gilbert <trgamma@yahoo.com>
2018-09-09 02:44:38 +0000

    Done.

--- index.js
+++ index.js
@@ -8,9 +8,9 @@ function happyHolidaysTo(name) {
 }
 
 function happyHolidayTo(holiday, name) {
-  return 'Happy ' + holiday + ', ' + name + '!';
+  return `Happy ${holiday}, ${name}!`;
 }
 
 function holidayCountdown(holiday, days) {
-  return 'It\'s ' + days + ' days until ' + holiday + '!';
+  return `It's ${days} days until ${holiday}!`;
 }
