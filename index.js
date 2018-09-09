d16819e9245e1f85d98cbb3235e8c7211c3090b5
billy@arkhamgc.com <billy@arkhamgc.com>
2018-09-09 17:04:07 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
 // write your code below!
+function happyHolidays(){
+  return "Happy holidays!";
+}
+
+function happyHolidaysTo(name){
+  return `Happy holidays, ${name}!`;
+}
+
+function happyHolidayTo(holiday, name){
+  return `Happy ${holiday}, ${name}!`;
+  
+}
+
+function holidayCountdown(holiday, days){
+  return `It's ${days} days until ${holiday}!`;
+  
+}
+
+
+  
+  
+
+
+
+
+
+
+
+
+
+
+//today=new Date();
+//var cmas=new Date(today.getFullYear(), 11, 25);
+//if (today.getMonth()==11 && today.getDate()>25) 
+//{
+//cmas.setFullYear(cmas.getFullYear()+1); 
+//}  
+//var one_day=1000*60*60*24;
+//console.log(Math.ceil((cmas.getTime()-today.getTime())/(one_day))+
+//" days left until Christmas!");
