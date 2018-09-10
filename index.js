049dbb52f9c09d1f319c5d5effeba870b7fb50a1
John McWatters <jo.a.mcw@gmail.com>
2018-09-10 03:51:27 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// write your code below!
+// write your code belwo
+function happyHolidays(){
+  return "Happy holidays!"
+}
+
+function happyHolidaysTo(name) {
+  return "Happy holidays, "+ name + "!"
+}
+
+function happyHolidayTo(holiday, name) {
+  return `Happy ${holiday}, ${name}!`
+}
+
+function holidayCountdown(name, number){
+  return `It's ${number} days until ${name}!`
+}
