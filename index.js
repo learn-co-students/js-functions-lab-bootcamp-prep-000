09a5432c4948482d731d203214fa5ea46bd73004
atkinsonholly@gmail.com <atkinsonholly@gmail.com>
2018-09-13 14:21:22 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // write your code below!
+function happyHolidays() {
+  return "Happy holidays!"
+}
+function happyHolidaysTo(string) {
+  return "Happy holidays" + ", " + string +"!"
+}
+function happyHolidayTo(string1, string2) {
+  return "Happy " + string1 + ", " + string2 + "!"
+}
+function holidayCountdown(string1, string2) {
+  return "It's " + string2 + " days until " + string1 + "!"
+}
