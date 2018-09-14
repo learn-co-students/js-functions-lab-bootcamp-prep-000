cd9d7cc7e3b2a8be82fd84ae52abd82e55999bc9
Roberto Romero <creativeworks27@icloud.com>
2018-09-14 20:02:45 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // write your code below!
+
+function happyHolidays() {
+  var happy = "Happy holidays!";
+return("Happy holidays!");
+}
+function happyHolidaysTo(Jhon) {
+  return(`Happy holidays, ${Jhon}!`);
+}
+function happyHolidayTo(Christmas,Jhon) {
+  return(`Happy ${Christmas}, ${Jhon}!`);
+}
+function holidayCountdown(Christmas,fifteen) {
+  return(`It's ${fifteen} days until ${Christmas}!`);
+}
