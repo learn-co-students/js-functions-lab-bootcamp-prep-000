70bd2050b7ec2df059f2321360932ab2b5f1ddac
manon jacquin <manon.jacquin@gmail.com>
2018-09-18 15:48:29 +0000

    Done.

--- index.js
+++ index.js
@@ -4,13 +4,16 @@ function happyHolidays(){
 }
 
 function happyHolidaysTo(name){
+  // return an expression including the parameter we passed in the function
   return `Happy holidays, ${name}!`
 }
 
 function happyHolidayTo(holiday, name){
-  return `Happy ${holidays}, ${name}!`
+  // return an expression with the two parameters we passed in the function
+  return `Happy ${holiday}, ${name}!`
 }
 
 function holidayCountdown(holiday, days){
-  return `It's ${days} until ${holiday}!`
+  // return an expression with the two parameters we passed in the function
+  return `It's ${days} days until ${holiday}!`
 }
