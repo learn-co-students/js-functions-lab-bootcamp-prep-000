d6035435206b94f3157fa2ea52825fb1d05fcc9b
omid saedi <omidsaedii@outlook.com>
2018-09-21 00:24:52 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // write your code below!
+function happyHolidays()
+{
+  return ("Happy holidays!");
+}
+
+ function happyHolidayTo(noruz,omid)
+ {
+   return (`Happy ${noruz}, ${omid}!`);
+ }
+ 
+ function happyHolidaysTo (omid)
+ {
+   return (`Happy holidays, ${omid}!`);
+ }
+
+function holidayCountdown(holiday="mother's day ",days= 20 )
+{
+  return(`It's ${days} days until ${holiday}!`);
+}
