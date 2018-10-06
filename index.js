a2ac937cdeedf183934fc0318e43e7180811f060
Mamadou Diallo <mamadou.diallo@wework.com>
2018-10-06 00:27:37 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // write your code below!
+function happyHolidays(name){
+  return 'Happy holidays!'
+
+}
+happyHolidays('Happy holidays!')
+  
+  
+
+function happyHolidaysTo( holiday, name){
+return "Happy holidays, you!"
+}
+happyHolidaysTo("Happy holidays, you!")
+
+
+happyHolidayTo ("'Independence Day', 'you'")
+function happyHolidayTo(holiday, name){
+  return  'Happy Independence Day, you!'
+}
+happyHolidayTo('Independence Day', 'you')
+
+ 
+ 
+ function holidayCountdown(holiday, days){
+   return "It's 20 days until Mother's Day!"
+   
+ }
+ holidayCountdown("Mother's Day")
+
