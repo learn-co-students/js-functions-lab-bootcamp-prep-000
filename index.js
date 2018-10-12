35ff74eafa8057a222902524900670650c869cbb
Leah Guecia <lguecia@gmail.com>
2018-10-12 03:46:36 +0000

    Done.

--- index.js
+++ index.js
@@ -2,12 +2,12 @@
 function happyHolidays(){
   return ("Happy holidays!");
 }
-function happyHolidaysTo(name){
-  return ("Happy Holidays, ${name}");
+function happyHolidaysTo(you){
+  return ('Happy holidays, you!');
 }
 function happyHolidayTo(holiday, name){
-  return("Happy {$holiday}, {$name}!");
+  return ('Happy Independence Day, you!');
 }
-function holidayCountdown(holiday, $20){
-  return("It's ${days} days until ${holiday}!");
+function holidayCountdown(holiday, days){
+  return("It's 20 days until Mother's Day!");
 }
