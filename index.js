8fad449ad4e3b7ca5e946458b9b2f17b2d790f86
Darnell V Mccord <darnellvmccord@gmail.com>
2018-10-13 01:38:05 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // write your code below!
+
+function happyHolidays(string) {
+return (string = 'Happy holidays!')
+}
+
+var name = 'Darnell'
+
+function happyHolidaysTo(name){
+  return `Happy holidays, ${name}!`
+}
+
+var holiday ='Hallowenn'
+function happyHolidayTo(holiday, name){
+  return `Happy ${holiday}, ${name}!`
+}
+
+var days = 19
+function holidayCountdown(holiday, days){
+  return `It's ${days} days until ${holiday}!`
+}
