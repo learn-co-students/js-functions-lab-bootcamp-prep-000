d31b48dc344dce5f01fbb7b839e1183028cbd3ca
Fei Qi <feiqi9047@gmail.com>
2018-11-09 16:23:11 +0000

    Done.

--- index.js
+++ index.js
@@ -7,16 +7,14 @@ function happyHolidays(){
 function happyHolidaysTo(name){
   return `Happy holidays, ${name}!`
 }
-happyHolidaysTo("you")
 
 
 function happyHolidaysTo(holiday, name){
   return `Happy ${holiday}, ${name}!`
 }
-happyHolidaysTo("Independence Day", "you")
+
 
 
 function holidayCountdown(days, holiday){
   return `It\'s ${days} days until ${holiday}!`
 }
-holidayCountdown(20, "Mother's Day")
