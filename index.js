53ecdbbd8cdee52427f74ffdcfeb3de845b4b502
Janette Wallace <jmariewallace2@yahoo.com>
2018-11-12 15:13:40 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// write your code below!
+// write your code below /
+var name = "you"
+function happyHolidays(string) {
+  return "Happy holidays!"
+}
+function happyHolidaysTo(string) {
+  return `Happy holidays, ${name}!`
+}
+function happyHolidayTo(string) {
+  var holiday = "Independence Day"
+  return `Happy ${holiday}, ${name}!`
+}
+function holidayCountdown(string) {
+  var holiday = "Mother's Day"
+  var days = "20"
+  return `It's ${days} days until ${holiday}!`
+}
