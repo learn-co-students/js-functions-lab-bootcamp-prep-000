bc5162fe8c0536cfa8d71f5163fdcfb87bdd87d5
Paris Dolfman <parisdolfman@gmail.com>
2018-11-27 20:27:41 +0000

    Done.

--- index.js
+++ index.js
@@ -7,3 +7,7 @@ function happyHolidays() {
 function happyHolidaysTo(name) {
   return `Happy holidays, ${name}!`
 }
+
+function happyHolidayTo(holiday, name) {
+  return `Happy ${holiday}, ${name}!`
+}
