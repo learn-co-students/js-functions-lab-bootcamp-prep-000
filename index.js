3bcf1d0964ad2b5ef35c0bbca20e2b33606e5fec
Amanda Poetker <amanda@strategytwelve.com>
2018-12-03 09:56:12 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // write your code below!
+function happyHolidays() {
+  console.log("Happy holidays!")
+}
+function happyHolidaysTo() {
+  console.log(`Happy holidays, ${name}!`)
+}
+function happyHolidayTo() {
+  console.log(`Happy ${holiday}, ${name}!`)
+}
+function holidayCountdown() {
+  console.log(`It's ${days} days until ${holiday}!`)
+}
