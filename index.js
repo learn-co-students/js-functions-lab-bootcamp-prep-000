8eebd545109c25f883b6e07227f7fe0dd96ded19
rnavedojr <rnavedojr@outlook.com>
2018-12-24 15:34:22 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // write your code below!
+var name = "Reynaldo";
+var holiday = "New Year";
+var countdown = "7";
+
+function happyHolidays() {
+  return "Happy holidays!";
+}
+
+function happyHolidaysTo(name) {
+  return "Happy holidays, you!";
+}
+
+function happyHolidayTo(holiday, name) {
+  return "Happy Independence Day, you!";
+}
+
+function holidayCountdown(countdown, holiday) {
+  return "It\'s 20 days until Mother's Day!"
+}
