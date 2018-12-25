e43bb17628d47904bf287c46242e87464698152c
kovner chalumo <kchalumo@gmail.com>
2018-12-25 03:32:46 +0000

    Done.

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
-function HappyHolidaysTo(name) { return 'Happy holidays, ${name}!'; }
-function HappyHolidays(name) { return "Happy, holidays!"; }
-function HappyHolidaysTo(holiday, name) { return 'Happy ${holiday}, ${name}!';
+function happyHolidays(name) { return 'Happy holidays!'; }
+function happyHolidaysTo(name) { return `Happy holidays, ${name}!`;}
+function holidayCountdown(holiday, days) { return `It\'s ${days} days until ${holiday}!`;
 }
-function holidayCountdown(days, holiday) { return 'It\'s ${days} days until ${holiday}!';
-}
+function happyHolidayTo(holiday, name) {return `Happy ${holiday}, ${name}!`;}
