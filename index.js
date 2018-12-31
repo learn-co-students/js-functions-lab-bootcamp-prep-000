9f7cbeec00220bcb3d6c35d3a7057a8fe069dee5
Blake Hardesty <ha1003218@email.ccbcmd.edu>
2018-12-31 20:01:41 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// write your code below!
+function happyHolidays() {
+  return "Happy holidays!";
+}
+
+function happyHolidaysTo(name) {
+  return "Happy holidays, you!";
+}
+
+function happyHolidayTo(holiday){
+  return "Happy Independence Day, you!"
+}
+
+function holidayCountdown(days){
+return "It\'s 20 days until Mother\'s Day!"
+}
