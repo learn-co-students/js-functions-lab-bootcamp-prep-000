be82bb4ce7e438d99c777c69b88bd51cf100d7b7
Grace Tan <gracetan37@gmail.com>
2019-01-23 08:10:14 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // write your code below!
+function happyHolidays() {
+  console.log('Happy holidays!')
+}
+Function happyHolidaysTo("name") {
+  console.log(`Happy holidays, ${name}!`)
+}
+Function happyHolidayTo("holiday", "name") {
+  console.log(`Happy ${holiday}, ${name}!`)
+}
