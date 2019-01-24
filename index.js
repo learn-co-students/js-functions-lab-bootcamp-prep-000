2ef94ba2775d05f9986cec8aa46b94a48cc5c4a9
kendall willard <kendallwillard@hotmail.com>
2019-01-24 23:32:31 +0000

    Done.

--- index.js
+++ index.js
@@ -10,3 +10,7 @@ function happyHolidaysTo(name) {
 function happyHolidayTo(holiday, name) {
   return 'Happy ${holiday}, ${name}!'
 }
+
+function holidayCountdown(days, holiday) {
+  return 'It\'s ${days} until ${holiday}!'
+}
