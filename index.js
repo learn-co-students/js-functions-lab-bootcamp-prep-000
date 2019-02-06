6449a0c0924138c892d13636f22af8bf51bc6541
Juniper A <juniper.alcorn@gmail.com>
2019-02-06 21:42:10 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // write your code below!
+
+function happyHolidays() {
+  return 'Happy holidays!'
+}
+
+var name
+name = 'you'
+
+function happyHolidaysTo(name) {
+  return `Happy holidays, ${name}!`
+}
+
+var holiday
+holiday = 'Independence Day'
+
+function happyHolidayTo(holiday, name) {
+  return `Happy ${holiday}, ${name}!`
+}
+
+holiday = 'Mother\'s Day'
+var days
+days = 20
+
+function holidayCountdown(holiday, days) {
+  return `It\'s ${days} days until ${holiday}!`
+}
