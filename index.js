28c5a412aa40267b7b21f65f1fa623fbbcee3042
Dan Kagan <dankagan7@gmail.com>
2019-02-10 23:24:51 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // write your code below!
+var countDown = `It's ${days} days until ${holidayTwo}!`
+var days = 20
+var greeting = `Happy holidays!`
+var greetingOther = `Happy holidays, you!`
+var happy = `Happy ${holidayOne}, ${name}!`
+var holidayOne = "Independence Day"
+var holidayTwo = "Mother's Day"
+var name = "you"
+
+
+function happyHolidays() {
+  return(greeting)
+}
+
+function happyHolidaysTo(name) {
+  return(greetingOther)
+}
+
+function happyHolidayTo(holidayOne, name) {
+  return(happy)
+}
+
+function holidayCountdown(days, holidayTwo) {
+  return(countDown)
+}
