fc5146a0cb9d1ae36445caf703b7875dc013fe4d
Rebecca Cairns <rebecca_cairns@yahoo.com>
2019-02-11 20:25:24 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// write your code below!
+function happyHolidays(){
+  return "Happy holidays!";
+}
+
+function happyHolidaysTo(name){
+  var printer = "Happy holidays, " + name + "!";
+  return printer;
+}
+function happyHolidayTo(holiday, name){
+  var printer = "Happy " + holiday + ", " + name + "!";
+  return printer;
+}
+function holidayCountdown(holiday, days){
+  var printer = "It\'s " + days + " days until " + holiday + "!";
+  return printer;
+}
