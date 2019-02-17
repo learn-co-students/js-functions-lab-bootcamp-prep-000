48111bc8d8d82ac79be54ce3427f0c3feeb66a74
Jean-Kenel Dessources <jkenel_23@yahoo.fr>
2019-02-17 03:35:17 +0000

    Done.

--- index.js
+++ index.js
@@ -6,3 +6,11 @@ function happyHolidays(){
 function happyHolidaysTo(name){
   return `Happy holidays, ${name}!`
 }
+
+function happyHolidayTo(holiday, name){
+  return `Happy ${holiday}, ${name}!`
+}
+
+function holidayCountdown(days, holiday){
+  return `It's ${holiday} days until ${days}!`
+}
