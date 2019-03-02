5261883aacd8be5bcaf454a8b3f4b8eba811c02d
Dondre Moore <raptureimmortal@icloud.com>
2019-03-02 20:55:20 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// write your code below!
+function happyHolidays() {
+return("Happy holidays!") 
+}
+function happyHolidaysTo(You) {
+  console.log('${You}!')
+  return 'Happy holidays, you!'
+}
+function happyHolidayTo(You, Mom) {
+  return 'Happy Independence Day, you!'
+}
+function holidayCountdown(holiday, days) {
+  return(`It's ${days} days until ${holiday}!`)
+}
+
