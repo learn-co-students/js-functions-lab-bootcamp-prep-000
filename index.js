ad0bcf8401c12d4aa361a9247f500a128b41bbd4
Christopher Cabell <cccabell@icloud.com>
2019-03-02 00:07:06 +0000

    Done.

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 function happyHolidays() {
-  return `Happy holidays!`
+  return `Happy holidays!`;
 }
 
 function happyHolidaysTo(name) {
-  return `Happy holidays, ${name}!`
+  return `Happy holidays, ${name}!`;
 }
 
 function happyHolidayTo(holiday, name) {
-  return `Happy ${holiday}, ${name}!`
+  return `Happy ${holiday}, ${name}!`;
 }
 
 function holidayCountdown(holiday, days) {
-  return `It's ${days} days until ${holiday}!`
+  return `It's ${days} days until ${holiday}!`;
 }
