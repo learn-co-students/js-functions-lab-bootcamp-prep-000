3a96ef726ad5714c21013a94cd565a7a6880c37f
sheila barron <sheilabarron@yahoo.com>
2019-03-03 03:29:04 +0000

    Done.

--- index.js
+++ index.js
@@ -1,12 +1,22 @@
 // write your code below!
-function happyHolidays() {
-  return ('Happy holidays!')
-}
 
-var name = "you"
+function happyHolidays() {
+  return "Happy holidays!";
+}
 
 function happyHolidaysTo(name) {
-  console.log ('Happy holidays, ${name}!')
+  return `Happy holidays, ${name}!`;
 }
 
-happyHolidaysTo(sheila)
+function happyHolidayTo(holiday, name) {
+  return `Happy ${holiday}, ${name}!`;
+}
+
+function holidayCountdown(holiday, days) {
+  return `It\'s ${days} days until ${holiday}!`;
+}
+
+happyHolidays();
+happyHolidaysTo("you");
+happyHolidayTo("Mother\'s Day", "you");
+holidayCountdown("20", "Mother\'s Day");
