7aa2232ba5c027b0955ae34354b645353f5bd7b8
merry-lab-3791 <emilyroulund@gmail.com>
2019-03-14 03:21:57 +0000

    Done.

--- index.js
+++ index.js
@@ -1,13 +1,19 @@
 // write your code below!
-function happyHolidays(greeting){
-  return "Happy holidays!"
+function happyHolidays(){
+  return "Happy holidays!";
 }
+//accept no parameters 
+
 function happyHolidaysTo(name){
-  return `Happy holidays, ${name}!`
+  return `Happy holidays, ${name}!`;
 }
+//accept parameter of name
+
 function happyHolidayTo(holiday, name){
   return `Happy ${holiday}, ${name}!`
 }
+//two parameters 
+
 function holidayCountdown(holiday, days){
   return `It's ${days} days until ${holiday}!`
 }
