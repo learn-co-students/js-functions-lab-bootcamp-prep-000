48eb2bd340975e2edd32dae05c0c1acab74be50b
Richard Li <richardli125@gmail.com>
2019-03-29 07:49:42 +0000

    Done.

--- index.js
+++ index.js
@@ -12,5 +12,5 @@ function happyHolidayTo(holiday, name) {
 }
 
 function holidayCountdown(holiday, days) {
-  return "It's ${days} days until ${holiday}!"
+  return 'It\'s ${days} days until ${holiday}!'
 }
