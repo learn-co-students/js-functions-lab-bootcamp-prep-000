dafbc008d120fead9edccb51a712456e1e12d69c
Emeka, Alex Odiah <alexodiah@gmail.com>
2019-04-03 16:55:39 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // write your code below!
+//Write a function named happyHolidays. 
+//This function should not accept any parameters and should
+//return the string "Happy holidays!"
+function happyHolidays(){
+return("Happy holidays!")
+}
+
+//Write a function named happyHolidaysTo. 
+//This function should accept a parameter of the name of the person you want to wish happy holidays, 
+//and return the string `Happy holidays, ${name}!`
+var name = "you",
+    holiday = "holiday";
+    
+function happyHolidaysTo(name) {
+  return(`Happy holidays, ${name}!`)
+}
+function happyHolidayTo(holiday, name) {
+return(`Happy ${holiday}, ${name}!`)
+}
+function holidayCountdown(holiday, days){
+  return(`It's ${days} days until Mother's Day!`)
+}
