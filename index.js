992039d4c8b6665b0a943857106aba433846e908
Danielle Martinoli <daniechris5@gmail.com>
2019-04-18 16:58:56 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // write your code below!
+function happyHolidays() {
+  return "Happy holidays!"
+}
+
+var name="you"
+var days= 20
+
+function happyHolidaysTo(){
+  return `Happy holidays, ${name}!`
+}
+
+function happyHolidayTo(){
+  return `Happy Independence Day, ${name}!`
+}
+
+function holidayCountdown(){
+  return `It's ${days} days until Mother's Day!`
+}
