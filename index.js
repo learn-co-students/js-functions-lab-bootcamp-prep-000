bfd55743e9d303de5ebb565debf303a59429aad2
Anais Trope <trope.anais@gmail.com>
2019-04-27 21:49:25 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // write your code below!
+
+/*Write a function named happyHolidays returning the string "Happy holidays" */
+function happyHolidays(){
+  return ("Happy holidays!")
+}
+
+/*Write a function named happyHolidaysTo with one parameter. */
+function happyHolidaysTo(name){
+  return `Happy holidays, ${name}!`
+}
+
+/*Write a function named happyHolidayTo with two parameters. */
+function happyHolidayTo(holiday, name){
+  return `Happy ${holiday}, ${name}!`
+}
+
+/*Write a function named holidayCountdown with two parameters. */
+function holidayCountdown(holiday, days){
+  return `It's ${days} days until ${holiday}!`
+}
