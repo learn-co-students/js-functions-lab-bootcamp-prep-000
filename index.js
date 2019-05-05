e0c01d228b64410347156b7d658108e6f73b96f7
exuberant-glitch-6816 <natalia.girolamo@gmail.com>
2019-05-05 01:06:48 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // write your code below!
+//1. Write a function that returns a value
+function happyHolidays(){
+  return "Happy holidays!";
+}
+//Write a function that takes in a parameter
+function happyHolidaysTo(name){
+  return `Happy holidays, ${name}!`;
+}
+//Write a function that takes in multiple parameters
+function happyHolidayTo(holiday, name){
+  return `Happy ${holiday}, ${name}!`;
+}
+//Write a function that takes in multiple parameters
+function holidayCountdown(holiday, days){
+  return `It's ${days} days until ${holiday}!`;
+}
+ 
