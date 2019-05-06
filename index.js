fbb3b30d45f46b12f65207a9e3777e055b205a93
Sharnell Grant <sharnellgrant@gmail.com>
2019-05-06 01:52:31 +0000

    Done.

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 // write your code below!
 function happyHolidays(string) {
-  var string = happyHolidays
-  if (string === Happy holidays!)
+  var string = Happy holidays!
   return "Happy holidays!"
 }
