0ea9e47aae6cbde5ac9f079eb84ed8cf64e3c741
Philip Roth <phil.roth077@gmail.com>
2019-05-28 02:59:02 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // write your code below!
+function happyHolidays(string) {
+  return "Happy holidays!"
+
+}
+
+function happyHolidaysTo(name) {
+  return "Happy holidays, you!"
+}
+
+function happyHolidayTo(string) {
+  return "Happy Independence Day, you!"
+}
+
+function holidayCountdown(string) {
+  return "It's 20 days until Mother's Day!"
+
+}
