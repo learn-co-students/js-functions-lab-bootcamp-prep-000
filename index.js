74f3cd68ebceec0d21d3b8b219c1bbc01278ec6b
benevolent-listener-0694 <pranas_29@yahoo.co.uk>
2019-05-29 18:49:00 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // write your code below!
+function happyHolidays(){
+  return "Happy holidays!"
+}
+function happyHolidaysTo(mika){
+  return  `Happy holidays, ${name}!`
+}
+function happyHollidayTo(june,mika){
+  return `Happy ${holiday}, ${name}!`
+}
+function holidayCountdown(junetime,sevendays){
+  return `It's ${days}, ${holiday}!`
+}
