908717c3b38d1d946ad007db79ab3d7c7b16c116
Nausherwan Khiljee <nausherwan.khiljee@gmail.com>
2019-06-02 02:29:44 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// write your code below!
+var index = require("./root.js")
+
+function happyHolidays() {
+  return "Happy holidays!"
+}
+
+function happyHolidaysTo(name) {
+  return `Happy holidays, ${name}!`
+}
+
+
+function happyHolidayTo(holiday, name) {
+  return `Happy ${holiday}, ${name}!`
+}
+
+function holidayCountdown(days, holiday) {
+  return "It's 20 days until Mother's Day!"
+}
