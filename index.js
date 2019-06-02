9a115a3a61f09655e35a65ef1d31ca4a7da61e6e
Brandon Fries <brandonfries1993@gmail.com>
2019-06-02 14:02:43 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
 // write your code below!
+function happyHolidays(string){
+  return ("Happy holidays!")
+}
+function happyHolidaysTo(name) {
+  return ('Happy holidays, you!')
+}
+function happyHolidayTo(holiday, name) {
+  return ('Happy Independence Day, you!')
+}
+function holidayCountdown(holiday, day) {
+  return ('It\'s 20 days until Mother\'s Day!')
+}
+
+describe('functions', () => {
+  describe('happyHolidays', () => {
+    it('returns "Happy holidays!"', () => {
+      expect(happyHolidays()).toEqual('Happy holidays!')
+    })
+  })
+
+  describe('happyHolidaysTo(name)', () => {
+    it('returns "Happy holidays, ${name}!"', () => {
+      expect(happyHolidaysTo('you')).toEqual('Happy holidays, you!')
+    })
+  })
+
+  describe('happyHolidayTo(holiday, name)', () => {
+    it('returns "Happy ${holiday}, ${name}!"', () => {
+      expect(happyHolidayTo('Independence Day', 'you')).toEqual('Happy Independence Day, you!')
+    })
+  })
+
+  describe('holidayCountdown(holiday, days)', () => {
+    it('returns "It\'s ${days} days until ${holiday}!"', () => {
+      expect(holidayCountdown("Mother's Day", 20)).toEqual("It's 20 days until Mother's Day!")
+    })
+  })
+})
