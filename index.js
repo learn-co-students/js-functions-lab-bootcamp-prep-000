ff786a86dc8df4283a0e13a9d44d095c0a363332
Justin L Rackham <justin.rackham@gmail.com>
2019-06-06 07:49:49 +0000

    Done.

--- index.js
+++ index.js
@@ -1,5 +1,37 @@
 // write your code below!
 
-function happyHolidays() {
+/*function happyHolidays() {
   return "Happy holidays!"
+}
+
+// Having trouble getting ${name} to call 'you'. Directions seem to be different. Initial thoughts were to call the function... happyHolidaysTo('you') outside of brackets
+function happyHolidaysTo(name)  {
+ 
+  return 'Happy holidays, you!'
+  
+}
+
+function happyHolidayTo(holiday, name) {
+  return "Happy Independence Day, you!"
+}
+
+function holidayCountdown(holiday, days) {
+  return "It's 20 days until Mother's Day!"
+}
+*/
+
+function happyHolidays () {
+  return "Happy holidays!";
+}
+
+function happyHolidaysTo (name) {
+  return `Happy holidays, ${name}!`;
+}
+
+function happyHolidayTo (holiday, name) {
+  return `Happy ${holiday}, ${name}!`;
+}
+
+function holidayCountdown (holiday, days) {
+  return `It's ${days} days until ${holiday}!`;
 }
