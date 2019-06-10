9856f8c7cd11e78a3ea0bf3c947bb8e32e2a02a9
Gian Moran <gpmoran@uh.edu>
2019-06-10 19:04:50 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // write your code below!
+function happyHolidays(){
+  var greeting = "Happy holidays!"
+  return greeting
+}
+
+function happyHolidaysTo(name){
+  //name = 'you'
+  return `Happy holidays, ${name}!`
+}
+//happyHolidaysTo(name)
+
+function happyHolidayTo(holiday,name){
+  
+  return `Happy ${holiday}, ${name}!`
+  }
+  function holidayCountdown(holiday,days){
+    return `It's ${days} days until ${holiday}!`
+  }
