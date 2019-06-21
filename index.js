412a076f850800a832bb757be6e72753919cc121
Richard Carpino <carpino82@gmail.com>
2019-06-21 11:52:16 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// write your code below!
+function happyHolidays(){
+  return "Happy holidays!"
+}
+
+function happyHolidaysTo(name) {
+  return 'Happy holidays, ${name}!'
+}
+
+function happyHolidaysTo(holiday, name) {
+  return 'Happy ${holiday}, ${name}!'
+}
