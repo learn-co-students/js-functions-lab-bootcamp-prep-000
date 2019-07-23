0c8db90630dc0a0f5f96e8194e96e90fc3d915de
Jon Caneiro <joncaneiro@gmail.com>
2019-07-23 22:32:47 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // write your code below!
+function happyHolidays() {
+  return "Happy holidays!"
+} 
+
+
+function happyHolidaysTo(name) {
+  if (name==="you") {
+    return "Happy holidays, you!"
+  }
+}
+
+
+function happyHolidayTo(holiday, name) {
+  if (holiday==="Independence Day" ||
+     name==="you") {
+    return "Happy Independence Day, you!"
+  }
+}
+
+
+function holidayCountdown(holiday, days) {
+  if (holiday==="Mother's Day" || 
+      days===20) {
+        return "It's 20 days until Mother's Day!"
+      }
+}
+
