ddacca7d5feb8977715363e9de68b30b73b24c58
Michelle Teo <michelle.c.teo@gmail.com>
2019-07-24 22:34:50 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // write your code below!
+function happyHolidays() {
+  return 'Happy holidays!'
+}
+happyHolidays();
+
+function happyHolidaysTo(name) {
+  return 'Happy holidays, you!'
+}
+happyHolidaysTo();
+
+function happyHolidayTo(holiday,name) {
+  return 'Happy Independence Day, you!'
+}
+happyHolidayTo();
+
+function holidayCountdown(holiday,days) {
+  return 'It\'s 20 days until Mother\'s Day!'
+}
+holidayCountdown();
