85097185dc8f8f4f6e72de7285f5255ed50220f5
Felix Kariuki <fkariuki1@live.co.uk>
2019-07-28 15:07:41 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// write your code below!
+function happyHolidays() {
+  return 'Happy holidays!'
+}
+
+function happyHolidaysTo(Flatiron) {
+  return `Happy holidays, ${Flatiron}!`
+}
+
+function happyHolidayTo(Easter, Flatiron) {
+  return `Happy ${Easter}, ${Flatiron}!`
+}
+
+function holidayCountdown(Christmas, days) {
+  return `It's ${20} days until ${Christmas}!`
+}
