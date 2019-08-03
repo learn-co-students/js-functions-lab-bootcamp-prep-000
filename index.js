4c73976cbee0d9579d55d55828f87dc830be383d
Connor Meth <connormeth27@gmail.com>
2019-08-03 19:43:23 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // write your code below!
+var name = "you";
+
+function happyHolidays(string) {
+  return "Happy holidays!";
+}
+function happyHolidaysTo(string) {
+
+  return `Happy holidays, ${name}!`;
+}
+function happyHolidayTo(string) {
+  var holiday = "Independence Day";
+  return `Happy ${holiday}, ${name}!`;
+}
+function holidayCountdown(string) {
+  var days = 20;
+  var holiday = "Mother\'s Day!";
+  return `It\'s ${days} days until ${holiday}`;
+}
