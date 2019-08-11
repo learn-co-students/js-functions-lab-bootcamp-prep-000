afe587fbe841f95106719b728111f0e8f6dcff4a
Anastasia Pankina <anastaciap@ymail.com>
2019-08-11 19:11:29 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // write your code below!
+//first one with no parameters
+
+function happyHolidays(){
+  return `Happy holidays!`;
+}
+
+//with name parameter 
+
+function happyHolidaysTo(name){
+  return `Happy holidays, ${name}!`;
+}
+
+//with 2 parameters: holiday and name
+
+function happyHolidayTo(holiday,name){
+  return `Happy ${holiday}, ${name}!`;
+}
+
+//with 2 parameters :the holday name and number of days till that holiday
+function holidayCountdown(holiday, days){
+  return `It's ${days} days until ${holiday}!`;
+  
+}
