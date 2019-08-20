93b1eee7065e151aa777f6c85dcb44a88b2fea11
sensible-gateway-6049 <greedybrain18@gmail.com>
2019-08-20 15:21:32 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // write your code below!
+function happyHolidays() {
+  return "Happy holidays!";
+}
+
+function happyHolidaysTo(holiday, name) {
+  return `Happy ${holiday}, ${name}`;
+}
+
+function happyHolidaysTo(holiday, days) {
+  return `It's ${days} days until ${holiday}`;
+}
