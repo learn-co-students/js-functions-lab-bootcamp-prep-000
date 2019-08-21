0c6a6cece909969528d6a9b5fc52e0ad96a5a079
Ioan Scurtu <ioan.scurtu120@gmail.com>
2019-08-21 15:49:12 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// write your code below!
+function (happyHolidays) {
+  retrun '${happyHolidays!}'
+}
+
+function (happyHolidaysTo) {
+  return 'Happy hollidays', '${you}!'
+}
+
+function (happyHolidayTo) {
+  'Happy ${holiday}, ${me}!'
+}
+
+function (holidayCountdown) {
+  " It's ${20} days until ${Mother's Day}!"
+}
