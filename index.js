3ea369b270853609b6312b5691d228e9de228d0f
Luis Cintron <l.cintron9966@outlook.com>
2019-08-31 18:32:43 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // write your code below!
+
+
+function happyHolidays() {
+  return 'Happy holidays!';
+}
+
+
+function happyHolidaysTo(hoilday, name) {
+  return "Happy Mother's Day, you!";
+}
+
+function happyHolidaysTo(name) {
+  return 'Happy holidays, you!';
+}
+
+
+
+function holidayCountdown(holiday, days) {
+  return "It's 20 days until Mother's Day!";
+}
+ 
+ 
+
