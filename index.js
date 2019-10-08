2b0def3c0572da60bcb472e03c16d4b63f6676ed
breezy-constructor-6081 <murtaza63@gmail.com>
2019-10-08 07:37:12 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // write your code below!
+function happyHolidays(){
+  return "Happy holidays!";
+}
+happyHolidays();
+
+function happyHolidaysTo(Raqeeb){
+  return `Happy holidays, ${Raqeeb}!`;
+}
+
+function happyHolidayTo(Eid, Raqeeb){
+  return `Happy ${Eid}, ${Raqeeb}!`;
+}
+
+function holidayCountdown(Eid, three){
+  return `It's ${three} days until ${Eid}!`;
+}
