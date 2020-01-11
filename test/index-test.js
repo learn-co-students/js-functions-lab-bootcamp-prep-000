e29c33e2ba526fb3967b1e3134d638fa683331ef
Jonathan T Dean <jonathantomdean@gmail.com>
2020-01-11 03:32:05 +0000

    Done.

--- test/index-test.js
+++ test/index-test.js
@@ -9,22 +9,21 @@ describe('functions', () => {
 
   describe('happyHolidaysTo(name)', () => {
     it('returns "Happy holidays, ${name}!"', () => {
-      expect(happyHolidaysTo('you')).toEqual('Happy holidays, you!')
+      expect(happyHolidaysTo('Jonathan')).toEqual('Happy holidays, Jonathan!')
       expect(happyHolidaysTo('everyone')).toEqual('Happy holidays, everyone!')
     })
   })
 
   describe('happyHolidayTo(holiday, name)', () => {
     it('returns "Happy ${holiday}, ${name}!"', () => {
-      expect(happyHolidayTo('Independence Day', 'you')).toEqual('Happy Independence Day, you!')
+      expect(happyHolidayTo('Halloween', 'Jonathan')).toEqual('Happy Halloween, Jonathan!')
       expect(happyHolidayTo('New Year', 'everyone')).toEqual('Happy New Year, everyone!')
     })
   })
 
   describe('holidayCountdown(holiday, days)', () => {
     it('returns "It\'s ${days} days until ${holiday}!"', () => {
-      expect(holidayCountdown("Mother's Day", 20)).toEqual("It's 20 days until Mother's Day!")
-      expect(holidayCountdown("Father's Day", 120)).toEqual("It's 120 days until Father's Day!")
+      expect(holidayCountdown("Halloween", 20)).toEqual("It's 20 days until Halloween!")
     })
   })
 })
