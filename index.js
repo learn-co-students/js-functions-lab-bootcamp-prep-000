8c4a3f40f81bc45bb535c9d3be6ae7a633afad6d
candescent-mouse-0917 <aqiph818@gmail.com>
2020-02-02 22:31:44 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// write your code below!
+// happyHolidays
+function happyHolidays(){
+  return 'Happy holidays!'
+}
+
+//happyHolidaysTo(name)
+function happyHolidaysTo(name){
+  return `Happy holidays, ${name}!`
+}
+
+//happyHolidayTo(holiday, name)
+function happyHolidayTo(holiday, name){
+  return `Happy ${holiday}, ${name}!`
+}
+
+//holidayCountdown(holiday, days)
+function holidayCountdown(holiday, days){
+  return `It's ${days} days until ${holiday}!`
+}
