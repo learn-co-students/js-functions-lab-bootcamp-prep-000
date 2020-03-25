9e249a6d32f9e02b952ec274355059b5cb9dc60d
Cat Marsh <c@catamarsh.com>
2020-03-25 02:22:34 +0000

    Done.

--- index.js
+++ index.js
@@ -1,6 +1,19 @@
 // write your code below!
 function happyHolidays() {
-  return("Happy holidays!")
+  return ("Happy holidays!")
 }
 
-function happyHolidaysTo(name)
+function happyHolidaysTo(name) {
+  return (`Happy holidays, ${name}!`)
+
+  happyHolidaysTo(you)
+  happyHolidaysTo(everyone)
+}
+
+function happyCustomHolidayTo(holiday, name) {
+  return (`Happy ${holiday}, ${name}!`)
+}
+
+function holidayCountdown(holiday, days) {
+  return (`It's ${days} days until ${holiday}!`)
+}
