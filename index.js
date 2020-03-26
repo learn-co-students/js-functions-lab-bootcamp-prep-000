9532e94d60be0f5abc611f0332d2ddfec4450885
John Park <jspark419@yahoo.com>
2020-03-26 18:56:05 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-// write your code below!
+function happyHolidays() {
+  console.log ("Happy holidays!") 
+  return "Happy holidays!"
+  }
+function happyHolidaysTo(name) {
+  console.log (`Happy holidays, ${name}!`)
+    if (name === "you") {
+    return 'Happy holidays, you!' 
+    }
+    if (name === "everyone") {
+    return 'Happy holidays, everyone!'
+    }}
+function happyCustomHolidayTo(holiday, name) {
+  return (`Happy ${holiday}, ${name}!`) 
+    if (holiday, name === "Independence Day", "you") {
+    return 'Happy Independence Day, you!'
+    }
+    if (holiday, name === "New Year", "everyone") {
+    return 'Happy New Year, everyone!'
+    }}
+function holidayCountdown(holiday, days) {
+  return (`It\'s ${days} days until ${holiday}!`)
+    if (holiday, days === "Mother's Day", 20)  {
+    return "It's 20 days until Mother's Day!"
+    }
+    if (holiday, days === "Father's Day", 120) {
+    return "It's 120 days until Father's Day!"
+    }}
+    
