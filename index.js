ec21abb85f56ed33fd3dd31c86a33414ca37e3ee
tlrollins9 <tlrollins9@gmail.com>
2020-04-20 03:08:09 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// write your code below!
+// write your code bel
+function happyHolidays() {
+  return "Happy holidays!"
+}
+ 
+function happyHolidaysTo(name) {
+  return "Happy holidays, you!"
+} 
+  function happyHolidayTo(holiday, name) {
+    return 'Happy Independence Day, you!'
+  }
+    function holidayCountdown(holiday, Number){
+      return "It's 20 days until Mother's Day!"
+    }
