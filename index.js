e7765ca2db64ada9a6472d6df284f23e74da6a6c
Victor Romero <romeroa.victor@yahoo.com>
2020-05-22 21:00:59 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// write your code below!
+function happyHolidays(){
+  return "Happy holidays!";
+}
+function happyHolidaysTo(Broly){
+   return `Happy holidays, ${Broly}!`;
+}
+function happyCustomHolidayTo(Birthday, Broly){
+  return `Happy ${Birthday}, ${Broly}!`;
+}
+function holidayCountdown(holiday, ten){
+  return `It's ${ten} days until ${holiday}!`;
+  }// write your code below!
