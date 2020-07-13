4f0bb38038ca773ab3475fa1c1f5f63901489e9c
Wanda Larosiliere <wglarosiliere@gmail.com>
2020-07-13 15:57:07 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,16 @@
+// 3 functions and 3 variables
+
+
+function happyHolidays()
+{
+  console.log("Happy holidays!");
+  return ("Happy holidays!");
+}
+
+var name = "you"
+function happyHolidaysTo(name)
+{
+  console.log("Happy holidays, "+ name + "!");
+  return (`Happy holidays, ${name}!`);
+}
+
