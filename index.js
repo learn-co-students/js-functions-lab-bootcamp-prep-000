c89b715cd3e87f02350a8e990e8009c10122bb17
scrappy-cyborg-4243 <itchkimel@gmail.com>
2020-07-14 18:16:51 +0000

    Done.

--- index.js
+++ index.js
@@ -1,4 +1,16 @@
 // write your code below!
-function happyHolidays()
-  retun happyHolidays
+function happyHolidays(){
+  return "Happy holidays!"
+}
   
+function happyHolidaysTo (name){
+  return `Happy holidays, ${name}!`
+}  
+
+function happyCustomHolidayTo (holiday, name){
+  return `Happy ${holiday}, ${name}!`
+}
+
+function holidayCountdown (holiday, days){
+  return `It's ${days} days until ${holiday}!`
+}
