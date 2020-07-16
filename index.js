c366dd179e9ad3dbae01c0316047679e7f96a05d
Hugo Delgado <hugo.delgado1@outlook.com>
2020-07-16 20:42:33 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // write your code below!
+/*
+const name = 'Leah';
+let holiday = "Mother's Day";
+let days = 20; */
+
+function happyHolidays() {
+  return 'Happy holidays!';
+};
+
+function happyHolidaysTo(name) {
+  return `Happy holidays, ${name}!`;
+};
+
+function happyCustomHolidayTo(holiday, name) {
+  return `Happy ${holiday}, ${name}!`;
+};
+
+function holidayCountdown(holiday, days) {
+  return `It's ${days} days until ${holiday}!`;
+};
