e8750dd2adb769ed3dedce9f18c0f102ce0060d7
Aurangzaib <aurangzaib.danial@gmail.com>
2020-07-27 02:42:16 +0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// write your code below!
+function happyHolidays() {
+  return "Happy holidays!"
+}
+
+function happyHolidaysTo(name) {
+  return `Happy holidays, ${name}!`
+}
+
+function happyCustomHolidayTo(holiday_name, person_name) {
+  return `Happy ${holiday_name}, ${person_name}!`
+}
+
+function holidayCountdown(holiday, days) {
+  return `It's ${days} days until ${holiday}!`
+}
