4e74936b0ebf5e49002c619203f74b4ba7d2495c
Reid T Deinlein <rtdeinlein5@gmail.com>
2020-08-04 16:02:26 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-// write your code below!
+var date = "July 3rd"
+console.log('My birthday is ${date}')
+function happyHolidays(){
+  return "Happy holidays!"}
+function happyHolidaysTo(name) {
+  return `Happy holidays, ${name}!`}
+function happyCustomHolidayTo(holiday, name){
+  return `Happy ${holiday}, ${name}!`}
+function holidayCountdown(holiday,days){
+  return `It\'s ${days} days until ${holiday}!`}
