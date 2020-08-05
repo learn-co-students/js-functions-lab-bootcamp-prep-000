7fa8cd6ef0c2d637d2f4b0babd1f554c024c524f
kind-prototype-8380 <dk2800@nyu.edu>
2020-08-05 19:24:37 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // write your code below!
+function happyHolidays() {
+return "Happy holidays!"
+}
+
+function happyHolidaysTo(Doug) {
+  return `Happy holidays, ${Doug}!`
+}
+
+function happyCustomHolidayTo(Christmas,Doug) {
+  return `Happy ${Christmas}, ${Doug}!`
+}
+
+function holidayCountdown(Christmas,ten) {
+  return `It's ${ten} days until ${Christmas}!`
+}
