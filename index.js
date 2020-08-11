428e407ddfaed9d62b2e39c78ed49af39ab0b1d9
Ghamdan Mohsin <chemist2ever@gmail.com>
2020-08-11 03:32:47 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // write your code below!
+
+function happyHolidaysTo(ghamdan)
+  return string.(`Happyholidays, ${name}!`)
+
+function happyHolidaysTo()()
+  return string.`Happy ${holiday}, ${name}!`
+
+function holidayCountdown()()
+return string.` It,s ${days} day until ${holiday}!`
