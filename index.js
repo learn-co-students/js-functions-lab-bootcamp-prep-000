6a1c7d5c23cc9b693c04d3ac939fb0934eb85e4c
Stephen Enke <stephenenke@gmail.com>
2020-08-17 21:52:46 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // write your code below!
+var name = "you";
+var holiday = "Independence Day";
+var days = 20;
+
+function happyHolidays() {
+  return "Happy holidays!"
+}
+function happyHolidaysTo(name) {
+  return `Happy holidays, ${name}!`
+}
+function happyCustomHolidayTo(holiday, name) {
+  return `Happy ${holiday}, ${name}!`
+}
+function holidayCountdown(holiday, days) {
+  return `It's ${days} days until ${holiday}!`
+}
