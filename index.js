2506f993db7d695cd824fa252883590538b0ce88
DariasKehl <neocount@gmail.com>
2020-09-29 14:01:08 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // write your code below!
+
+function happyHolidays() { 
+    //console.log("Keep the change, ya filthy animal!");
+    return 'Happy holidays!'
+}
+
+function happyHolidaysTo(name) {
+    return `Happy holidays, ${name}!`;
+}
+
+function happyCustomHolidayTo(holiday, name) {
+    //console.log('I\'m your hucklebery...'); //Tombstone, Val Kilmer
+    return `Happy ${holiday}, ${name}!`
+}
+function holidayCountdown(holiday, days) {
+    return `It's ${days} days until ${holiday}!`}
+
+
