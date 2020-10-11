4e4492fade00e1dad7cbc0c9ed8fcb38080caf80
celestial-applet-1261 <zihan991@gmail.com>
2020-10-11 16:00:02 +0000

    Done.

--- index.js
+++ index.js
@@ -1,16 +1,14 @@
-// write your code below!
 function happyHolidays() {
 return "Happy holidays!";  
 }
+
 function happyHolidaysTo(name) {
-  return `Happy holidays, ${name}!`;
+return `Happy holidays, ${name}!`;  
 }
-happyHolidaysTo('name');
-function happyCustomHolidayTo(holiday, name) {
-  return `Happy ${holiday}, ${name}!`;
+
+function happyCustomHolidayTo(holiday, name){
+return `Happy ${holiday}, ${name}!`;  
 }
-happyCustomHolidayTo('holiday', 'name');
 function holidayCountdown(holiday, days) {
-return `it's ${days} days until ${holiday}`;
+return `It's ${days} days until ${holiday}!`;  
 }
-holidayCountdown('holiday', 'days');
