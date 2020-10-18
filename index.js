03e5ff142b396046b2dccf703f6294b70167b63a
Roy Frias <roy.frias2@gmail.com>
2020-10-18 04:06:31 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // write your code below!
+// var name = 'Roy';
+// var holiday = 'Independence Day';
+var days = 'four';
+
+function happyHolidays() {
+  return 'Happy holidays!';
+}
+
+function happyHolidaysTo(name) {
+  return `Happy holidays, ${name}!`;
+}
+
+function happyCustomHolidayTo(holiday, name) {
+  return `Happy ${holiday}, ${name}!`;
+}
+
+function holidayCountdown(holiday, days) {
+  return `It's ${days} days until ${holiday}!`;
+}
