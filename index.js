8724e27135144f372d04404d891c7163c02960e5
astronomical-queue-4656 <halierae17@outlook.com>
2020-11-07 20:39:12 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // write your code below!
+function happyHolidays() {
+  return "Happy holidays!";
+}
+var name = "you"
+function happyHolidaysTo(name) {
+  return `Happy holidays, ${name}!`;
+}
+var holiday = "christmas"
+function happyCustomHolidayTo(holiday, name) {
+  return `Happy ${holiday}, ${name}!`;
+}
+var days = "2"
+function holidayCountdown(holiday, days) {
+  return `It's ${days} days until ${holiday}!`;
+}
