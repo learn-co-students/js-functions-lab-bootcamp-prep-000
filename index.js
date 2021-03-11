6249ce2e4c05b7e2c83d0d74a1839cba76123465
Thyda Korng <thydakorng@gmail.com>
2021-03-11 15:20:46 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // write your code below!
+
+function happyHolidays(String) {
+  return "Happy holidays!"
+}
+
+function happyHolidaysTo(name = Thyda) {
+  return `Happy holidays, ${name}!`
+}
+
+function happyCustomHolidayTo(holiday = Christmas, name = Thyda) {
+  return `Happy ${holiday}, ${name}!`
+}
+
+function holidayCountdown(holiday = Thanksgiving, days = 100) {
+  return `It's ${days} days until ${holiday}!`
+}
