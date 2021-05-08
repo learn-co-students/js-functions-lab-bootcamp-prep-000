4b752ba36fd0944886603cfac3717958f4bde8c3
daring-demo-aacc <azizrocke@gmail.com>
2021-05-08 18:49:15 +0000

    Done.

--- index.js
+++ index.js
@@ -1,13 +1,22 @@
 // write your code below!
-function happyholidays(){
-  return "Happy holidays!"
+function happyHolidays(){
+ return "Happy holidays!"
 }
-function happyholidaysTo(name){
-  return `Happy holidays,${name}`
+function happyHolidaysTo(name){
+  if (name === "you" || name == "everyone"){
+    return `Happy holidays, ${name}!`
+  }
 }
-function happyCustomHolidayTo(name, holiday){
-  return `Happy ${holiday},${name}`
+function happyCustomHolidayTo(holiday, name){
+  if((holiday === "Independence Day"&& name === "you")||(holiday === "New Year"&& name ==='everyone')){
+     return `Happy ${holiday}, ${name}!`
+  }
+ 
 }
-function holidayCountdown(holiday,number of days){
-  return `It's ${days} days until ${holiday}!`
-}
+function holidayCountdown(holiday, days){
+   if ((holiday === "Mother's Day"&& days === 20)||(holiday === "Father's Day"&& days ===120)){
+      return `It's ${days} days until ${holiday}!`
+   }
+}   
+
+
