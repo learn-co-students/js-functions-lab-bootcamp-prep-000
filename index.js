6bd0da87d47a3bffd0af791e1371d57e834d7df5
MAGICTNT <78156199+MAGICTNT@users.noreply.github.com>
2021-10-09 12:25:32 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// write your code below!
+
+function happyHolidays()
+{
+  return 'Happy holidays!';
+}
+console.log(happyHolidays());
+
+
+function happyHolidaysTo( holiday='October (Hacktober fest)' , name='Max')
+{
+  return 'Happy '+ holiday +', '+ name +'!';
+}
+console.log(happyHolidaysTo());
+
+
+function holidayCountdown(holidayName ='November', numberDaysUntilHoliday ='22')
+{
+  return 'It\'s '+ numberDaysUntilHoliday +' days until '+ holidayName +'!';
+}
+console.log(holidayCountdown());
